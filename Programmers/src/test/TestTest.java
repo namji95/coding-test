3320ad747cf422facac57e53b79a89a903beefd2
namji <skawlgns07@naver.com>
2023-11-07 21:24:03 +0900

    first commit message

--- Programmers/src/test/TestTest.java
+++ Programmers/src/test/TestTest.java
@@ -3,5 +3,7 @@ package test;
 public class TestTest {
 	public static void main(String[] args) {
 		System.out.println("Hello JAVA");
+		System.out.println("Hello JAVA");
+		System.out.println("Hello JAVA");
 	}
 }
