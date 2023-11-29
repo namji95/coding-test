909c7e710b43058d9743afafd044f8d5d8a31e12
namji <skawlgns07@naver.com>
2023-11-29 22:17:20 +0900

    2023.11.29 프로그래머스
    매개변수 문자열 my_string과 정수 k
    my_string을 k번 반복하여 출력
    for문을 이용하여 문자열을 k만큼 순회하여 더하도록 하여 출력하는 방법과
    String클래스의 repeat 메소드를 이용하는 방법으로 문제 해결

---
+++ Programmers/src/com/namji/codingtest/StringMultiplication.java
@@ -0,0 +1,19 @@
+package com.namji.codingtest;
+
+public class StringMultiplication {
+	public static void main(String[] args) {
+		/*
+		 * 문자열 my_string과 정수 k가 매개변수로 주어집니다.
+		 * my_string을 k번 반복한 값을 출력하는 문제입니다.
+		 * "string", 3 >	"stringstringstring"
+		 * "love", 10 >	"lovelovelovelovelovelovelovelovelovelove"
+		 */
+		String my_string = "love";
+		int k = 10;
+		System.out.println(my_string.repeat(k));
+//		String answer = "";
+//		for (int i = 0; i < k; i++) {
+//			answer += my_str;
+//		}
+	}
+}
