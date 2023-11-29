c69055bdf471de2e9a3671a09d9fd2e28dd75c87
namji <skawlgns07@naver.com>
2023-11-29 21:35:49 +0900

    2023.11.29 프로그래머스
    매개변수 정수로 이루어진 문자열 n_str
    n_str의 가장 왼쪽의 0을 모두 제외하고 출력
    n_str을 정수로 변환하고 다시 문자열로 변환하는 방법과
    n_str을 정수로 변환함과 동시에 빈 문자열을 더하는 방법으로 해결

---
+++ Programmers/src/com/namji/codingtest/Remove_0.java
@@ -0,0 +1,16 @@
+package com.namji.codingtest;
+
+public class Remove_0 {
+	public static void main(String[] args) {
+		/*
+		 * 정수로 이루어진 문자열 n_str이 매개변수로 주어집니다.
+		 * n_str의 가장 왼쪽에 처음으로 등장하는 0은 모두 제외하고 출력하는 문제입니다.
+		 * "0010" > "10" / "854020" > "854020"
+		 */
+		String n_str = "0010";
+		System.out.println("" + Integer.parseInt(n_str));
+//		int n = Integer.parseInt(n_str);
+//		String answer = String.valueOf(n);
+//		System.out.println(answer);
+	}
+}
