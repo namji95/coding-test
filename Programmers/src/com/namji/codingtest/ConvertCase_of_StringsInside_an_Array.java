87f7b6a08c42faff1f065aa95b2d06cf1d91c3a8
namji <skawlgns07@naver.com>
2023-12-12 18:09:34 +0900

    2023.12.12 프로그래머스
    매개변수로 문자열 배열 strArr이 주어집니다.
    배열 안의 모든 원소는 알파벳으로만 이루어져 있습니다.
    배열의 홀수 인덱스 문자열은 모두 대문자로,
    배열의 짝수 인덱스 문자열은 모두 소문자로
    변환하여 출력하는 문제입니다.
    우선 for문을 이용하여 strArr의 크기만큼 순회하도록 하고
    if문을 사용하여 strArr의 인덱스가 짝수라면
    그 원소를 소문자로 변환 후 answer 배열에 담고
    그게 아니라면 그 인덱스의 원소를 대문자로 변환하여 answer에 담도록 했습니다.
    일반적인 if문을 사용한 코드와 삼항 연산자를 이용한 코드로 풀어봤습니다.

---
+++ Programmers/src/com/namji/codingtest/ConvertCase_of_StringsInside_an_Array.java
@@ -0,0 +1,30 @@
+package com.namji.codingtest;
+
+import java.util.Arrays;
+
+public class ConvertCase_of_StringsInside_an_Array {
+	public static void main(String[] args) {
+		/*
+		 * 문자열 배열 strArr이 매개변수로 주어집니다.
+		 * 모든 원소가 알파벳으로만 이루어져 있습니다.
+		 * 배열의 홀수 인덱스 문자열은 모두 대문자로,
+		 * 배열의 짝수 인덱스 문자열은 모두 소문자로
+		 * 변환하여 출력하는 문제입니다.
+		 * ["AAA","BBB","CCC","DDD"] > ["aaa","BBB","ccc","DDD"]
+		 * ["aBc","AbC"] > ["abc","ABC"]
+		 */
+		String[] strArr = {"aBc","AbC"};
+		String[] answer = new String[strArr.length];
+//		for (int i = 0; i < strArr.length; i++) {
+//			if (i % 2 == 0) {
+//				answer[i] = strArr[i].toLowerCase();
+//			} else {
+//				answer[i] = strArr[i].toUpperCase();
+//			}
+//		}
+		for (int i = 0; i < strArr.length; i++) {
+			answer[i] = i % 2 == 0 ? strArr[i].toLowerCase() : strArr[i].toUpperCase();
+		}
+		System.out.println(Arrays.toString(answer));
+	}
+}
