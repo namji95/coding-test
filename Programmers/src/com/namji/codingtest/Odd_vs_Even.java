bb426f811f327ff586324a4ec7528b2b58b8ab20
namji <skawlgns07@naver.com>
2023-12-28 21:06:25 +0900

    2023.12.28 프로그래머스
    정수 리스트 num_list가 매개변수로 주어집니다.
    정수 리스트의 첫 번째 원소를 1번 원소라고 할 때,
    홀수 번째 원소들의 합과
    짝수 번째 원소들의 합 중
    큰 값을 출력하는 문제입니다.
    
    짝수 번째 원소를 담기 위한 even, 홀수 번째 원소를 담기 위한 odd라는 변수를 선언합니다.
    이제 for문을 이용하여 num_list를 순회합니다.
    그 안에서 if문을 이용하여 짝수 번째와 홀수 번째를 나눠서 값을 변수에 담습니다.
    대신 배열의 시작과 for문의 시작이 0이기 때문에 i에 +1을 해주어 1부터 시작하는 것처럼 해줍니다.
    짝수 번쨰와 홀수 번째의 값을 담은 변수를 if문을 사용하여 값을 출력하면 됩니다.
    저는 for문 뒤에는 삼항 연산자를 이용하여 풀었습니다.

---
+++ Programmers/src/com/namji/codingtest/Odd_vs_Even.java
@@ -0,0 +1,29 @@
+package com.namji.codingtest;
+
+public class Odd_vs_Even {
+	public static void main(String[] args) {
+		/*
+		 * 정수 리스트 num_list가 매개변수로 주어집니다.
+		 * 정수 리스트의 첫 번째 원소를 1번 원소라고 할 때,
+		 * 홀수 번째 원소들의 합과 
+		 * 짝수 번째 원소들의 합 중 
+		 * 큰 값을 출력하는 문제입니다.
+		 * [4, 2, 6, 1, 7, 6] > 17 / [-1, 2, 5, 6, 3] > 8
+		 */
+		int [] num_list = {4, 2, 6, 1, 7, 6};
+		int answer = 0;
+		int even = 0;
+		int odd = 0;
+		for (int i = 0; i < num_list.length; i++) {
+			if ((i+1) % 2 == 0) {
+				even += num_list[i];
+			} else {
+				odd += num_list[i];
+			}
+		}
+		
+		answer = even > odd ? even : odd;
+		
+		System.out.println(answer);
+	}
+}
