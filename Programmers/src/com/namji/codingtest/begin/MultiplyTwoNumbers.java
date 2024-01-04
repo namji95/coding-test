db939936e735480dfa36cedbd5a3034dafd68c35
namji <skawlgns07@naver.com>
2024-01-04 10:47:34 +0900

    2024.01.04 프로그래머스 입문
    정수 num1과 num2가 매개변수로 주어집니다.
    num1과 num2의 곱을 출력하는 문제입니다.
    
    num1 * num2

---
+++ Programmers/src/com/namji/codingtest/begin/MultiplyTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.namji.codingtest.begin;
+
+public class MultiplyTwoNumbers {
+	public static void main(String[] args) {
+		/*
+		 * 정수 num1과 num2가 매개변수로 주어집니다.
+		 * num1과 num2의 곱을 출력하는 문제입니다.
+		 * 3, 4 > 12 / 27, 19 > 513
+		 */
+		int num1 = 3;
+		int num2 = 4;
+		System.out.println(num1 * num2);
+	}
+}
