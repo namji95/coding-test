fc41be87269ccb2c5a5248206f4206bdb587beb4
namji <skawlgns07@naver.com>
2024-01-05 10:04:32 +0900

    2024.01.05 프로그래머스 입문
    정수 angle이 매개변수로 주어집니다.
    angle이 0 ~ 90도 미만은 1을
    angle이 90도는 2를
    angle이 91 ~ 180도 미만은 3을
    angle이 180도는 4를 출력하는 문제입니다.
    
    if문을 사용하여 값을 비교하고 if문 안에 조건에 맞게 범위를 설정해줬습니다.

---
+++ Programmers/src/com/namji/codingtest/begin/Protractor.java
@@ -0,0 +1,26 @@
+package com.namji.codingtest.begin;
+
+public class Protractor {
+	public static void main(String[] args) {
+		/*
+		 * 정수 angle이 매개변수로 주어집니다.
+		 * angle이 0 ~ 90도 미만은 1을
+		 * angle이 90도는 2를
+		 * angle이 91 ~ 180도 미만은 3을
+		 * angle이 180도는 4를 출력하는 문제입니다.
+		 * 70 > 1 / 91 > 3 / 180 > 4
+		 */
+		int angle = 70;
+		int answer = 0;
+		if (0 < angle && angle < 90) {
+	        answer = 1;
+	    } else if (angle == 90) {
+	        answer = 2;
+	    } else if (90 < angle && angle < 180) {
+	        answer = 3;
+	    } else if (angle == 180) {
+	        answer = 4;
+	    }
+		System.out.println(answer);
+	}
+}
