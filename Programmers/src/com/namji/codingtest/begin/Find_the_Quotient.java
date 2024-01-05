0b82d953a18b8bcc2fe9ec614dcaabd42472c668
namji <skawlgns07@naver.com>
2024-01-05 10:02:18 +0900

    2024.01.05 프로그래머스 입문
    정수 num1과 num2가 매개변수로 주어집니다.
    num1을 num2로 나눈 몫을 구하는 문제입니다.
    
    프로그래밍에서 나눈 몫을 구하는 기호는 / 이걸로 계산하면 됩니다.
    num1 / num2

---
+++ Programmers/src/com/namji/codingtest/begin/Find_the_Quotient.java
@@ -0,0 +1,14 @@
+package com.namji.codingtest.begin;
+
+public class Find_the_Quotient {
+	public static void main(String[] args) {
+		/*
+		 * 정수 num1과 num2가 매개변수로 주어집니다.
+		 * num1을 num2로 나눈 몫을 구하는 문제입니다.
+		 * 10, 5 > 2 / 7, 2 > 3
+		 */
+		int num1 = 10;
+		int num2 = 5;
+		System.out.println(num1 / num2);
+	}
+}
