30454a1063523d1a12b1dba4f873e0b2a8c527a0
NAMJI <138655542+namji95@users.noreply.github.com>
2024-02-01 18:35:09 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,13 +1,15 @@
 class Solution {
     public int solution(String myString, String pat) {
-        String s = "";
-        for (int i = 0; i < myString.length(); i++) {
-            if (myString.substring(i, i+1).equals("A")) {
-                s += "B";
-            } else {
-                s += "A";
-            }
-        }
-        return s.contains(pat) ? 1 : 0;
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        return myString.contains(pat) ? 1 : 0;
+        // String s = "";
+        // for (int i = 0; i < myString.length(); i++) {
+        //     if (myString.substring(i, i+1).equals("A")) {
+        //         s += "B";
+        //     } else {
+        //         s += "A";
+        //     }
+        // }
+        // return s.contains(pat) ? 1 : 0;
     }
 }
