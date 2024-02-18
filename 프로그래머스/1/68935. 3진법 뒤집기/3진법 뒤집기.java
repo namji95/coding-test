5f62e917d86bdc5242d89771761b562c08dfb3b1
NAMJI <138655542+namji95@users.noreply.github.com>
2024-02-19 00:09:04 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,11 +1,27 @@
 class Solution {
     public int solution(int n) {
-        String threeN = Integer.toString(n, 3);
-        StringBuilder reverse = new StringBuilder();
-        for (int i = threeN.length() -1; i >= 0; i--) {
-            reverse.append(threeN.charAt(i));
-        }
-        int newN = Integer.parseInt(String.valueOf(reverse), 3);
-        return newN;
+        
+        String third = Integer.toString(n,3);
+        StringBuilder sb = new StringBuilder(third);
+        String reverse = sb.reverse().toString();
+        
+        return Integer.parseInt(reverse, 3);
+
+//        String threeN = Integer.toString(n, 3);
+//        StringBuilder reverse = new StringBuilder();
+//        for (int i = threeN.length() -1; i >= 0; i--) {
+//            reverse.append(threeN.charAt(i));
+//        }
+//        int newN = Integer.parseInt(String.valueOf(reverse), 3);
+//        return newN;
+
+//        String a = "";
+//
+//        while (n > 0) {
+//            a = (n % 3) + a;
+//            n /= 3;
+//        }
+//        a = new StringBuilder(a).reverse().toString();
+//        return Integer.parseInt(a, 3);
     }
 }
