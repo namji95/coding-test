db5d272cd3d834fd1036bc721983d61c9b46c563
NAMJI <138655542+namji95@users.noreply.github.com>
2024-02-19 00:40:57 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] solution(int n, int m) {
-        int max = 0;
         int[] answer = new int[2];
         for (int i = 1; i <= n && i <= m; i++) {
             if (n % i == 0 && m % i == 0) {
-                max = i;
+                answer[0] = i;
+                answer[1] = (n * m) / i;
             }
         }
-        answer[0] = max;
-        answer[1] = (n * m) / max;
         return answer;
     }
 }
