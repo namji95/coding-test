53987da5d0fadd9757f91e6da9c98a57e9a6e2a3
NAMJI <138655542+namji95@users.noreply.github.com>
2024-02-19 11:24:46 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,13 +1,24 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        String[] ss = s.split("");
+//        String answer = "";
+//        String[] ss = s.split("");
+        char[] c = s.toCharArray();
         int idx = 0;
         
-        for (String str : ss) {
-            idx = str.contains(" ") ? 0 : idx + 1;
-            answer += idx % 2 == 0 ? str.toLowerCase() : str.toUpperCase();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == ' ') {
+                idx = 0;
+            } else {
+                c[i] = (idx++ % 2 == 0 ? Character.toUpperCase(c[i]) : Character.toLowerCase(c[i]));
+            }
         }
+        
+        return String.valueOf(c);
+        
+//        for (String str : ss) {
+//            idx = str.contains(" ") ? 0 : idx + 1;
+//            answer += idx % 2 == 0 ? str.toLowerCase() : str.toUpperCase();
+//        }
         
 //        for (int i = 0; i < ss.length; i++) {
 //            if (ss[i].equals(" ")) {
@@ -21,6 +32,6 @@ class Solution {
 //            }
 //            answer += ss[i];
 //        }
-        return answer;
+//        return answer;
     }
 }
