70b352ef2317b5e2b70161698d5f8afae6db0c6a
NAMJI <138655542+namji95@users.noreply.github.com>
2024-03-06 23:47:53 +0900

    [level 1] Title: 최소직사각형, Time: 1.73 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -3,12 +3,16 @@ class Solution {
         int answer = 0;
         int max_w = 0;
         int min_h = 0;
-        for (int i = 0; i < sizes.length; i++) {
-            int w = Math.max(sizes[i][0], sizes[i][1]);
-            int h = Math.min(sizes[i][0], sizes[i][1]);
-            max_w = Math.max(max_w, w);
-            min_h = Math.max(min_h, h);
+        for (int[] card : sizes) {
+            max_w = Math.max(max_w, Math.max(card[0], card[1]));
+            min_h = Math.max(min_h, Math.min(card[0], card[1]));
         }
+        // for (int i = 0; i < sizes.length; i++) {
+        //     int w = Math.max(sizes[i][0], sizes[i][1]);
+        //     int h = Math.min(sizes[i][0], sizes[i][1]);
+        //     max_w = Math.max(max_w, w);
+        //     min_h = Math.max(min_h, h);
+        // }
         return answer = max_w * min_h;
     }
 }
