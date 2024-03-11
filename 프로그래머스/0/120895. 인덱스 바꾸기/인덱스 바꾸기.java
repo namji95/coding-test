c0953812facda3a3c021112b3827cdd71f44b5ff
NAMJI <138655542+namji95@users.noreply.github.com>
2024-03-12 02:27:56 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.17 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,9 +1,14 @@
+import java.util.*;
+
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String answer = "";
-        char[] arr = my_string.toCharArray();
-        arr[num1] = my_string.charAt(num2);
-        arr[num2] = my_string.charAt(num1);
-        return answer = String.valueOf(arr);
+        List<String> list = new ArrayList<>(List.of(my_string.split("")));
+        Collections.swap(list, num1, num2);
+        return String.join("", list);
+        // String answer = "";
+        // char[] arr = my_string.toCharArray();
+        // arr[num1] = my_string.charAt(num2);
+        // arr[num2] = my_string.charAt(num1);
+        // return answer = String.valueOf(arr);
     }
 }
