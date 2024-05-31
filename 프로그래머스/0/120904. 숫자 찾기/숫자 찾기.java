cbd0528a398d39b81f6c4e65fe1b8f569c0ebaed
NAMJI <138655542+namji95@users.noreply.github.com>
2024-05-31 16:02:40 +0900

    [level 0] Title: 숫자 찾기, Time: 10.08 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,16 +1,18 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        String[] strNum = String.valueOf(num).split("");
+        return ("-" + num).indexOf(String.valueOf(k));
+        
+//         int answer = 0;
+//         String[] strNum = String.valueOf(num).split("");
 
-        for (int i = 0; i < strNum.length; i++) {
-            if (strNum[i].equals(String.valueOf(k))) {
-                answer = i+1;
-                break;
-            } else {
-                answer = -1;
-            }
-        }
-        return answer;
+//         for (int i = 0; i < strNum.length; i++) {
+//             if (strNum[i].equals(String.valueOf(k))) {
+//                 answer = i+1;
+//                 break;
+//             } else {
+//                 answer = -1;
+//             }
+//         }
+//         return answer;
     }
 }
