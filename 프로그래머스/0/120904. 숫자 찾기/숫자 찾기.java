d31f8ce7ee32fab192ac33fa638672fc1939e671
NAMJI <138655542+namji95@users.noreply.github.com>
2024-05-31 15:57:32 +0900

    [level 0] Title: 숫자 찾기, Time: 0.17 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        String[] strNum = String.valueOf(num).split("");
+
+        for (int i = 0; i < strNum.length; i++) {
+            if (strNum[i].equals(String.valueOf(k))) {
+                answer = i+1;
+                break;
+            } else {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
