6e4f5fa3881fb3cb25b27dd50d79a1ac8a4e84f7
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-02 18:52:57 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.52 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,13 +1,22 @@
 import java.util.*;
 
 class Solution {
-    public StringBuilder solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
-        char[] charArr = my_string.toLowerCase().toCharArray();
-        Arrays.sort(charArr);
-        for (char c : charArr) {
-            answer.append(c);
-        }
-        return answer;
+    public String solution(String my_string) {
+        char[] c = my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        
+        return new String(c);
     }
 }
+
+// class Solution {
+//     public StringBuilder solution(String my_string) {
+//         StringBuilder answer = new StringBuilder();
+//         char[] charArr = my_string.toLowerCase().toCharArray();
+//         Arrays.sort(charArr);
+//         for (char c : charArr) {
+//             answer.append(c);
+//         }
+//         return answer;
+//     }
+// }
