6e33b7629250490d475637188fcd4223f0ad0dca
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-04 09:51:21 +0900

    [level 0] Title: 수 조작하기 2, Time: 708.10 ms, Memory: 395 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,19 +1,40 @@
 class Solution {
-    public StringBuilder solution(int[] numLog) {
-        StringBuilder answer = new StringBuilder();
+    public String solution(int[] numLog) {
+        String answer = "";
         
-        for (int i = 1; i < numLog.length; i++) {
-            if (numLog[i] == numLog[i - 1] + 1) {
-                answer.append("w");
-            } else if (numLog[i] == numLog[i - 1] - 1) {
-                answer.append("s");
-            } else if (numLog[i] == numLog[i - 1] + 10) {
-                answer.append("d");
-            } else if (numLog[i] == numLog[i - 1] - 10) {
-                answer.append("a");
-            }
-        }
+    for (int i = 1; i < numLog.length; i++) {
+      int j = numLog[i] - numLog[i - 1];
+      switch (j) {
+        case 1 : answer += 'w';
+        break;
+        case -1 : answer += 's';
+        break;
+        case 10 : answer += 'd';
+        break;
+        case -10 : answer += 'a';
+      }
+    }
         
         return answer;
     }
 }
+
+// class Solution {
+//     public StringBuilder solution(int[] numLog) {
+//         StringBuilder answer = new StringBuilder();
+        
+//         for (int i = 1; i < numLog.length; i++) {
+//             if (numLog[i] == numLog[i - 1] + 1) {
+//                 answer.append("w");
+//             } else if (numLog[i] == numLog[i - 1] - 1) {
+//                 answer.append("s");
+//             } else if (numLog[i] == numLog[i - 1] + 10) {
+//                 answer.append("d");
+//             } else if (numLog[i] == numLog[i - 1] - 10) {
+//                 answer.append("a");
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
