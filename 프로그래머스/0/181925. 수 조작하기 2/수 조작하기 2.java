e2ae603e335131f08778a4dd9d3752ea35193f20
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-04 09:45:19 +0900

    [level 0] Title: 수 조작하기 2, Time: 9.79 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public StringBuilder solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 1; i < numLog.length; i++) {
+            if (numLog[i] == numLog[i - 1] + 1) {
+                answer.append("w");
+            } else if (numLog[i] == numLog[i - 1] - 1) {
+                answer.append("s");
+            } else if (numLog[i] == numLog[i - 1] + 10) {
+                answer.append("d");
+            } else if (numLog[i] == numLog[i - 1] - 10) {
+                answer.append("a");
+            }
+        }
+        
+        return answer;
+    }
+}
