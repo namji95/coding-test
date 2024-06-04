5cdddc8c0213462ae1fcf4d8732e9ece8083a22c
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-04 10:11:10 +0900

    [level 0] Title: 수 조작하기 2, Time: 687.45 ms, Memory: 375 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -2,22 +2,43 @@ class Solution {
     public String solution(int[] numLog) {
         String answer = "";
         
-    for (int i = 1; i < numLog.length; i++) {
-      int j = numLog[i] - numLog[i - 1];
-      switch (j) {
-        case 1 : answer += 'w';
-        break;
-        case -1 : answer += 's';
-        break;
-        case 10 : answer += 'd';
-        break;
-        case -10 : answer += 'a';
-      }
-    }
+        for (int i = 1; i < numLog.length; i++) {
+            int j = numLog[i] - numLog[i - 1];
+            switch (j) {
+                case 1 : answer += 'w';
+                    break;
+                case -1 : answer += 's';
+                    break;
+                case 10 : answer += 'd';
+                    break;
+                case -10 : answer += 'a';
+            }
+        }
         
         return answer;
     }
 }
+
+// class Solution {
+//     public StringBuilder solution(int[] numLog) {
+//         StringBuilder answer = new StringBuilder();
+
+//         for (int i = 1; i < numLog.length; i++) {
+//             int j = numLog[i] - numLog[i - 1];
+//             switch (j) {
+//                 case 1 : answer.append("w");
+//                     break;
+//                 case -1 : answer.append("s");
+//                     break;
+//                 case 10 : answer.append("d");
+//                     break;
+//                 case -10 : answer.append("a");
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
 
 // class Solution {
 //     public StringBuilder solution(int[] numLog) {
