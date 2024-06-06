4929262cebeff58e70c3e0dcb7ae37ffdac40246
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-06 18:21:33 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,20 +1,37 @@
 class Solution {
     public int solution(int a, int b, int c) {
+        int answer = a + b + c;
         
         if (a == b && b == c) {
-            return (a+b+c) * mathTwoPow(a, b, c) * mathThreePow(a, b, c);
-        } else if (a != b && b != c && a != c) {
-            return a + b + c;
-        } else {
-            return (a+b+c) * mathTwoPow(a, b, c);
+            answer =
+                answer *
+                (a * a + b * b + c * c) *
+                (a * a * a + b * b * b + c * c * c);
+        } else if (a == b || b == c || a == c) {
+            answer = answer * (a * a + b * b + c * c);
         }
-    }
-    
-    private static int mathTwoPow(int a, int b, int c) {
-        return (int) (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
-    }
 
-    private static int mathThreePow(int a, int b, int c) {
-        return (int) (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+        return answer;
     }
 }
+
+// class Solution {
+//     public int solution(int a, int b, int c) {
+        
+//         if (a == b && b == c) {
+//             return (a+b+c) * mathTwoPow(a, b, c) * mathThreePow(a, b, c);
+//         } else if (a != b && b != c && a != c) {
+//             return a + b + c;
+//         } else {
+//             return (a+b+c) * mathTwoPow(a, b, c);
+//         }
+//     }
+    
+//     private static int mathTwoPow(int a, int b, int c) {
+//         return (int) (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+//     }
+
+//     private static int mathThreePow(int a, int b, int c) {
+//         return (int) (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+//     }
+// }
