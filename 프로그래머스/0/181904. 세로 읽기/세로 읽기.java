2f4e0ddd0aab3adf51d90bc694ef3cf2e72f5278
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-07 20:00:30 +0900

    [level 0] Title: 세로 읽기, Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -3,15 +3,20 @@ import java.util.*;
 class Solution {
     public StringBuilder solution(String my_string, int m, int c) {
         StringBuilder answer = new StringBuilder();
-        List<String> newStr = new ArrayList<>();
         
-        for (int i = 0; i < my_string.length(); i += m) {
-            newStr.add(my_string.substring(i, i + m));
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            answer.append(my_string.charAt(i));
         }
         
-        for (String s : newStr) {
-            answer.append(s.charAt(c-1));
-        }
+//         List<String> newStr = new ArrayList<>();
+        
+//         for (int i = 0; i < my_string.length(); i += m) {
+//             newStr.add(my_string.substring(i, i + m));
+//         }
+        
+//         for (String s : newStr) {
+//             answer.append(s.charAt(c-1));
+//         }
         
         return answer;
     }
