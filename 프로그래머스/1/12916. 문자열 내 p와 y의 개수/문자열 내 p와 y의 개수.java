884399908431a8877d5fce2e6baea6d8c62290d3
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-10 12:22:53 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.31 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -2,19 +2,39 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
         String[] str = s.toLowerCase().split("");
-        int pNum = 0;
-        int yNum = 0;
+        int count = 0;
         
         for (int i = 0; i < str.length; i++) {
             if (str[i].equals("p")) {
-                pNum++;
+                count++;
             } else if (str[i].equals("y")) {
-                yNum++;
+                count--;
             }
         }
         
-        answer = pNum == yNum;
+        answer = count == 0;
 
         return answer;
     }
 }
+
+// class Solution {
+//     boolean solution(String s) {
+//         boolean answer = true;
+//         String[] str = s.toLowerCase().split("");
+//         int pNum = 0;
+//         int yNum = 0;
+        
+//         for (int i = 0; i < str.length; i++) {
+//             if (str[i].equals("p")) {
+//                 pNum++;
+//             } else if (str[i].equals("y")) {
+//                 yNum++;
+//             }
+//         }
+        
+//         answer = pNum == yNum;
+
+//         return answer;
+//     }
+// }
