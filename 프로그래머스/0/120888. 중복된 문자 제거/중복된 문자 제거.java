ba586bdc655569783312d77bb48d24afd5b1565b
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-11 15:16:11 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.43 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,19 +1,28 @@
 import java.util.*;
 
 class Solution {
-    public StringBuilder solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
-        Set<Character> set = new HashSet<>();
+    public String solution(String my_string) {
+        String[] answer = my_string.split("");
+        Set<String> set = new LinkedHashSet<String>(Arrays.asList(answer));
         
-        for (int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            
-            if (!set.contains(c)) {
-                set.add(c);
-                answer.append(c);
-            }
-        }
-        
-        return answer;
+        return String.join("", set);
     }
 }
+
+// class Solution {
+//     public StringBuilder solution(String my_string) {
+//         StringBuilder answer = new StringBuilder();
+//         Set<Character> set = new HashSet<>();
+        
+//         for (int i = 0; i < my_string.length(); i++) {
+//             char c = my_string.charAt(i);
+            
+//             if (!set.contains(c)) {
+//                 set.add(c);
+//                 answer.append(c);
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
