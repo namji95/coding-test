9a63626606bb0a8ae147e7b11f612f6ce6e394a7
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-14 22:25:06 +0900

    [level 0] Title: 문자열 섞기, Time: 0.23 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,12 +1,28 @@
 class Solution {
     public StringBuilder solution(String str1, String str2) {
         StringBuilder answer = new StringBuilder();
+        String[] s1 = str1.split("");
+        String[] s2 = str2.split(""); 
         
-        for (int i = 0; i < str1.length(); i++) {
-            answer.append(str1.charAt(i));
-            answer.append(str2.charAt(i));
+        for (int i = 0; i < s1.length; i++) {
+            answer.append(s1[i]);
+            answer.append(s2[i]);
         }
+        
         
         return answer;
     }
 }
+
+// class Solution {
+//     public StringBuilder solution(String str1, String str2) {
+//         StringBuilder answer = new StringBuilder();
+        
+//         for (int i = 0; i < str1.length(); i++) {
+//             answer.append(str1.charAt(i));
+//             answer.append(str2.charAt(i));
+//         }
+        
+//         return answer;
+//     }
+// }
