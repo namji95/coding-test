e2479f15daf9d734b4d394a7f776c701fba62ee3
NAMJI <138655542+namji95@users.noreply.github.com>
2024-06-17 17:04:45 +0900

    [level 0] Title: 글자 지우기, Time: 0.11 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,16 +1,45 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
-        String[] str = my_string.split("");
-        String answer = "";
+        StringBuilder answer = new StringBuilder(my_string);
         
-        for (int i = 0; i < indices.length; i++) {
-            str[indices[i]] = "";
+        for (int i : indices) {
+            answer.setCharAt(i, ' ');
         }
         
-        for (String s : str) {
-            answer += s;
-        }
-        
-        return answer;
+        return answer.toString().replace(" ", "");
     }
 }
+
+// class Solution {
+//     public StringBuilder solution(String my_string, int[] indices) {
+//         String[] str = my_string.split("");
+//         StringBuilder answer = new StringBuilder();
+        
+//         for (int i = 0; i < indices.length; i++) {
+//             str[indices[i]] = "";
+//         }
+        
+//         for (String s : str) {
+//             answer.append(s);
+//         }
+        
+//         return answer;
+//     }
+// }
+
+// class Solution {
+//     public String solution(String my_string, int[] indices) {
+//         String[] str = my_string.split("");
+//         String answer = "";
+        
+//         for (int i = 0; i < indices.length; i++) {
+//             str[indices[i]] = "";
+//         }
+        
+//         for (String s : str) {
+//             answer += s;
+//         }
+        
+//         return answer;
+//     }
+// }
