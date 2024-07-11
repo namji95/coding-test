5e4e756063217f4d57906a3f4b453119e33b8f1d
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-11 18:11:56 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -2,13 +2,20 @@ class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
         
+        // for (int i : num_list) {
+        //     while (i > 1) {
+        //         if (i % 2 == 0) {
+        //             i = i / 2;
+        //         } else {
+        //             i = (i - 1) / 2;
+        //         }
+        //         answer++;
+        //     }
+        // }
+        
         for (int i : num_list) {
-            while (i > 1) {
-                if (i % 2 == 0) {
-                    i = i / 2;
-                } else {
-                    i = (i - 1) / 2;
-                }
+            while (i != 1) {
+                i = i / 2;
                 answer++;
             }
         }
