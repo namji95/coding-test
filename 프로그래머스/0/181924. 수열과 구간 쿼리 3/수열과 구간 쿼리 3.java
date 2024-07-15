53ef2432f277d6c02c86cbb102c469a53575fc82
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-15 19:12:47 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,17 +1,37 @@
 import java.util.*;
 
 class Solution {
-    public List solution(int[] arr, int[][] queries) {
-        List<Integer> answer = new ArrayList<>();
-        
-        for (int i : arr) {
-            answer.add(i);
-        }
+    public int[] solution(int[] arr, int[][] queries) {
+        int i = 0;
+        int j = 0;
+        int temp = 0;
+        int[] answer = Arrays.copyOf(arr, arr.length);
         
         for (int[] query : queries) {
-            Collections.swap(answer, query[0], query[1]);
+            i = query[0];
+            j = query[1];
+            temp = answer[i];
+            
+            answer[i] = answer[j];
+            answer[j] = temp;
         }
         
         return answer;
     }
 }
+
+// class Solution {
+//     public List solution(int[] arr, int[][] queries) {
+//         List<Integer> answer = new ArrayList<>();
+        
+//         for (int i : arr) {
+//             answer.add(i);
+//         }
+        
+//         for (int[] query : queries) {
+//             Collections.swap(answer, query[0], query[1]);
+//         }
+        
+//         return answer;
+//     }
+// }
