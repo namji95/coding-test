929beaa8da813d3c9efda887d86c34b675655d46
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-18 14:48:24 +0900

    [level 0] Title: 세 개의 구분자, Time: 43.43 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,23 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public List solution(String myStr) {
-        List<String> answer = new ArrayList<>();
-        myStr = myStr.replace("a", " ");
-        myStr = myStr.replace("b", " ");
-        myStr = myStr.replace("c", " ");
-        String[] newStr = myStr.split(" ");
-
-        for (String s : newStr) {
-            if (!s.equals("")) {
-                answer.add(s);
-            }
-        }
+    public String[] solution(String myStr) {
+        myStr = myStr.replaceAll("[abc]+", ",");
+        myStr = myStr.charAt(0) == ',' ? myStr.substring(1) : myStr;
+        myStr = myStr.isEmpty() ? "EMPTY" : myStr;
         
-        if (answer.size() == 0) {
-            answer.add("EMPTY");
-        }
-        
-        return answer;
+        return myStr.split(",");
     }
 }
+
+// class Solution {
+//     public List solution(String myStr) {
+//         List<String> answer = new ArrayList<>();
+//         myStr = myStr.replace("a", " ");
+//         myStr = myStr.replace("b", " ");
+//         myStr = myStr.replace("c", " ");
+//         String[] newStr = myStr.split(" ");
+
+//         for (String s : newStr) {
+//             if (!s.equals("")) {
+//                 answer.add(s);
+//             }
+//         }
+        
+//         if (answer.size() == 0) {
+//             answer.add("EMPTY");
+//         }
+        
+//         return answer;
+//     }
+// }
