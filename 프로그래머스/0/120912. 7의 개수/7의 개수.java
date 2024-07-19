f62e5f34209231fa2deb7388d024b253ccd3490a
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-19 14:58:12 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -1,16 +1,32 @@
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        StringBuilder sb = new StringBuilder();
         
         for (int j : array) {
-            sb.append(j);
-        }
-        for (String s : sb.toString().split("")) {
-            if (Integer.parseInt(s) == 7) {
-                answer++;
+            while (j > 0) {
+                if (j % 10 == 7) {
+                    answer++;
+                }
+                j /= 10;
             }
         }
         return answer;
     }
 }
+
+// class Solution {
+//     public int solution(int[] array) {
+//         int answer = 0;
+//         StringBuilder sb = new StringBuilder();
+        
+//         for (int j : array) {
+//             sb.append(j);
+//         }
+//         for (String s : sb.toString().split("")) {
+//             if (Integer.parseInt(s) == 7) {
+//                 answer++;
+//             }
+//         }
+//         return answer;
+//     }
+// }
