4f41fb01575da44a6b25bc9264e3b7e16f967487
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-22 16:33:08 +0900

    [level 0] Title: 리스트 자르기, Time: 0.07 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,29 +1,64 @@
 import java.util.*;
 
+// class Solution {
+//     public List solution(int n, int[] slicer, int[] num_list) {
+//         List<Integer> answer = new ArrayList<>();
+//         int a = slicer[0];
+//         int b = slicer[1];
+//         int c = slicer[2];
+
+//         if (n == 1) {
+//             for (int i = 0; i <= b; i++) {
+//                 answer.add(num_list[i]);
+//             }
+//         } else if (n == 2) {
+//             for (int i = a; i < num_list.length; i++) {
+//                 answer.add(num_list[i]);
+//             }
+//         } else if (n == 3) {
+//             for (int i = a; i <= b; i++) {
+//                 answer.add(num_list[i]);
+//             }
+//         } else {
+//             for (int i = a; i <= b; i+=c) {
+//                 answer.add(num_list[i]);
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
-    public List solution(int n, int[] slicer, int[] num_list) {
-        List<Integer> answer = new ArrayList<>();
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> arr = new ArrayList<>();
+        int[] answer = {};
         int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
 
         if (n == 1) {
             for (int i = 0; i <= b; i++) {
-                answer.add(num_list[i]);
+                arr.add(num_list[i]);
             }
         } else if (n == 2) {
             for (int i = a; i < num_list.length; i++) {
-                answer.add(num_list[i]);
+                arr.add(num_list[i]);
             }
         } else if (n == 3) {
             for (int i = a; i <= b; i++) {
-                answer.add(num_list[i]);
+                arr.add(num_list[i]);
             }
         } else {
             for (int i = a; i <= b; i+=c) {
-                answer.add(num_list[i]);
+                arr.add(num_list[i]);
             }
         }
+        
+        answer = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i);
+        }
+        
         return answer;
     }
 }
