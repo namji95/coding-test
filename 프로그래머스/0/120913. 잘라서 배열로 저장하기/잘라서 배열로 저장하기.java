cdf86f7a84b2f5445f1734d43f27b53bf881b338
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-24 17:51:50 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -1,21 +1,43 @@
 import java.util.*;
 
+// class Solution {
+//     public List solution(String my_str, int n) {
+//         int j = 0;
+//         List<String> answer = new ArrayList<String>();
+        
+//         if (my_str.length() % n != 0) {
+//             j = my_str.length() / n + 1;
+//         } else {
+//             j = my_str.length() / n;
+//         }
+        
+//         for (int i = 0; i < j; i++) {
+//             if ((i + 1) * n < my_str.length()) {
+//                 answer.add(my_str.substring(i * n, (i + 1) * n));
+//             } else {
+//                 answer.add(my_str.substring(i * n));
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
-    public List solution(String my_str, int n) {
-        int j = 0;
-        List<String> answer = new ArrayList<String>();
+    public String[] solution(String my_str, int n) {
+        String[] answer = {};
         
         if (my_str.length() % n != 0) {
-            j = my_str.length() / n + 1;
+            answer = new String[my_str.length() / n + 1];
         } else {
-            j = my_str.length() / n;
+            answer = new String[my_str.length() / n];
         }
         
-        for (int i = 0; i < j; i++) {
+        for (int i = 0; i < answer.length; i++) {
             if ((i + 1) * n < my_str.length()) {
-                answer.add(my_str.substring(i * n, (i + 1) * n));
+                answer[i] = my_str.substring(i * n, (i + 1) * n);
             } else {
-                answer.add(my_str.substring(i * n));
+                answer[i] = my_str.substring(i * n);
             }
         }
         
