17103803b5ee74769da2f4578840c0087e995865
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-25 16:32:29 +0900

    [level 0] Title: 커피 심부름, Time: 1.65 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,31 +1,37 @@
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        
         for (String s : order) {
-            if (s.contains("cafelatte")) {
-                answer += 5000;
-            } else {
-                answer += 4500;
-            }
+            answer += s.contains("cafelatte") ? 5000 : 4500;
         }
-        
         return answer;
     }
 }
 
 // class Solution {
 //     public int solution(String[] order) {
-//         int answer = 0;
-        
+//         int answer = 0;  
+//         for (String s : order) {
+//             if (s.contains("cafelatte")) {
+//                 answer += 5000;
+//             } else {
+//                 answer += 4500;
+//             }
+//         }       
+//         return answer;
+//     }
+// }
+
+// class Solution {
+//     public int solution(String[] order) {
+//         int answer = 0;       
 //         for (int i = 0; i < order.length; i++) {
 //             if (order[i].contains("cafelatte")) {
 //                 answer += 5000;
 //             } else {
 //                 answer += 4500;
 //             }
-//         }
-        
+//         }    
 //         return answer;
 //     }
 // }
