c2c8d8b09b24aecdb1dec972d0d389bff26d78c3
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-27 18:05:02 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 4.85 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,20 +1,33 @@
 import java.util.*;
 
+// class Solution {
+//     public int[] solution(int[] arr, int[][] queries) {
+//         int[] answer = Arrays.copyOf(arr, arr.length);
+//         int s, e, k = 0;
+        
+//         for (int i = 0; i < queries.length; i++) {
+//             s = queries[i][0];
+//             e = queries[i][1];
+//             k = queries[i][2];
+//             for (int j = 0; j < arr.length; j++) {
+//                 if (s <= j && e >= j && j % k == 0) {
+//                     answer[j] += 1;
+//                 }
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = Arrays.copyOf(arr, arr.length);
-        int s, e, k = 0;
-        
         for (int i = 0; i < queries.length; i++) {
-            s = queries[i][0];
-            e = queries[i][1];
-            k = queries[i][2];
-            for (int j = 0; j < arr.length; j++) {
-                if (s <= j && e >= j && j % k == 0) {
-                    answer[j] += 1;
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                if (j % queries[i][2] == 0) {
+                    arr[j] += 1;
                 }
             }
         }
-        return answer;
+        return arr;
     }
 }
