ae680825e94f286e1d8bc5f74467015ebd98a220
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-29 11:41:52 +0900

    [level 0] Title: qr code, Time: 0.12 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,11 +1,23 @@
+// class Solution {
+//     public StringBuilder solution(int q, int r, String code) {
+//         StringBuilder answer = new StringBuilder();
+        
+//         for (int i = 0; i < code.length(); i++) {
+//             if (i % q == r) {
+//                 answer.append(code.charAt(i));
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
     public StringBuilder solution(int q, int r, String code) {
         StringBuilder answer = new StringBuilder();
         
-        for (int i = 0; i < code.length(); i++) {
-            if (i % q == r) {
-                answer.append(code.charAt(i));
-            }
+        for (int i = r; i < code.length(); i += q) {
+            answer.append(code.charAt(i));
         }
         
         return answer;
