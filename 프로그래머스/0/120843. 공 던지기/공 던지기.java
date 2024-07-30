e970c8b64a11d2d3398d8b9a463bf28ef9bff2cb
NAMJI <138655542+namji95@users.noreply.github.com>
2024-07-30 16:12:01 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,5 +1,11 @@
+// class Solution {
+//     public int solution(int[] numbers, int k) {
+//         return numbers[2 * (k - 1) % numbers.length];
+//     }
+// }
+
 class Solution {
     public int solution(int[] numbers, int k) {
-        return numbers[2 * (k - 1) % numbers.length];
+        return 2 * (k - 1) % numbers.length + 1;
     }
 }
