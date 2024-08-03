ac4582a9b227dbcbd8bf9c82a7f7ce9739a942df
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-03 21:06:20 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.08 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -1,20 +1,37 @@
+// class Solution {
+//     public int[] solution(String my_string) {
+//         String[] s = my_string.split("");
+//         String[] alphabet = {
+//             "A", "B", "C", "D", "E", "F", "G", "H", "I",
+//             "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+//             "S", "T", "U", "V", "W", "X", "Y", "Z",
+//             "a", "b", "c", "d", "e", "f", "g", "h", "i",
+//             "j", "k", "l", "m", "n", "o", "p", "q", "r",
+//             "s", "t", "u", "v", "w", "x", "y", "z"};
+//         int[] answer = new int[52];
+        
+//         for (int i = 0; i < s.length; i++) {
+//             for (int j = 0; j < alphabet.length; j++) {
+//                 if (s[i].equals(alphabet[j])) {
+//                     answer[j] += 1;
+//                 }
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] solution(String my_string) {
-        String[] s = my_string.split("");
-        String[] alphabet = {
-            "A", "B", "C", "D", "E", "F", "G", "H", "I",
-            "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-            "S", "T", "U", "V", "W", "X", "Y", "Z",
-            "a", "b", "c", "d", "e", "f", "g", "h", "i",
-            "j", "k", "l", "m", "n", "o", "p", "q", "r",
-            "s", "t", "u", "v", "w", "x", "y", "z"};
         int[] answer = new int[52];
         
-        for (int i = 0; i < s.length; i++) {
-            for (int j = 0; j < alphabet.length; j++) {
-                if (s[i].equals(alphabet[j])) {
-                    answer[j] += 1;
-                }
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (c >= 'a') {
+                answer[c - 'a' + 26]++;
+            } else {
+                answer[c - 'A']++;
             }
         }
         
