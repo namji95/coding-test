5ed44cc4c97bd1650cbc025f6112dc3353d8b739
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-05 10:26:12 +0900

    [level 0] Title: 두 수의 합, Time: 607.14 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,7 +1,20 @@
+// import java.math.*;
+
+// class Solution {
+//     public String solution(String a, String b) {
+//         return (new BigInteger(a).add(new BigInteger(b))) + "";
+//     }
+// }
+
 import java.math.*;
 
 class Solution {
-    public String solution(String a, String b) {
-        return (new BigInteger(a).add(new BigInteger(b))) + "";
+    public StringBuilder solution(String a, String b) {
+        StringBuilder answer = new StringBuilder();
+        BigInteger A = new BigInteger(a);
+        BigInteger B = new BigInteger(b);
+        answer.append(A.add(B));
+        
+        return answer;
     }
 }
