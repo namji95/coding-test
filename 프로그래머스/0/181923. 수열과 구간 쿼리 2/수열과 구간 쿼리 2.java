d56d9f8535970f5386e08a78d45d67b8372666ab
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-09 17:46:27 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 5.60 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,21 +1,42 @@
+// import java.util.*;
+
+// class Solution {
+//     public List solution(int[] arr, int[][] queries) {
+//         List<Integer> answer = new ArrayList<>();
+        
+//         for (int[] query : queries) {
+//             int temp = 1000000;
+//             int s = query[0], e = query[1], k = query[2];
+//             for (int i = s; i <= e; i++) {
+//                 if (arr[i] > k) {
+//                     temp = Math.min(temp, arr[i]);
+//                 }
+//             }
+//             if (temp == 1000000){
+//                 temp = -1;
+//             }
+//             answer.add(temp);
+//         }
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
-    public List solution(int[] arr, int[][] queries) {
-        List<Integer> answer = new ArrayList<>();
-        
-        for (int[] query : queries) {
-            int temp = 1000000;
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, -1);
+
+        for (int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
             int s = query[0], e = query[1], k = query[2];
-            for (int i = s; i <= e; i++) {
-                if (arr[i] > k) {
-                    temp = Math.min(temp, arr[i]);
+            for (int j = s; j <= e; j++) {
+                if (arr[j] > k) {
+                    answer[i] = answer[i] == -1 ? arr[j] : Math.min(answer[i], arr[j]);
                 }
             }
-            if (temp == 1000000){
-                temp = -1;
-            }
-            answer.add(temp);
         }
         
         return answer;
