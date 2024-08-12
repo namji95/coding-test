a104424ae002c3ae785977066d52607372a3b800
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-12 15:57:15 +0900

    [level 0] Title: 외계어 사전, Time: 0.17 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2; int idx = 0;
+        
+        for (int i = 0; i < dic.length; i++) {
+            idx = 0;
+            for (int j = 0; j < spell.length; j++) {
+                if (dic[i].contains(spell[j])) {
+                    idx++;
+                }
+                if (idx == spell.length) {
+                    answer = 1;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
