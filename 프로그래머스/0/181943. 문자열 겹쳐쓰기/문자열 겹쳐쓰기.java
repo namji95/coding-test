2e5a6a962048042b656a53a1688a0d08cd5d311b
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-20 09:09:12 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.10 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,14 +1,25 @@
+// class Solution {
+//     public StringBuilder solution(String my_string, String overwrite_string, int s) {
+//         StringBuilder answer = new StringBuilder();
+        
+//         for (int i = 0; i < s; i++) {
+//             answer.append(my_string.charAt(i));
+//         }
+//         answer.append(overwrite_string);
+//         if (answer.length() < my_string.length()) {
+//             answer.append(my_string.substring(answer.length()));
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public StringBuilder solution(String my_string, String overwrite_string, int s) {
         StringBuilder answer = new StringBuilder();
         
-        for (int i = 0; i < s; i++) {
-            answer.append(my_string.charAt(i));
-        }
-        answer.append(overwrite_string);
-        if (answer.length() < my_string.length()) {
-            answer.append(my_string.substring(answer.length()));
-        }
-        return answer;
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        
+        return answer.append(before).append(overwrite_string).append(after);
     }
 }
