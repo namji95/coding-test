61c2bcf94ce3205dc8cba43bddcc7d0a07371669
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-21 10:26:03 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.79 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,14 +1,38 @@
+// import java.util.*;
+
+// public class Solution {
+//     public List solution(int []arr) {
+//         List<Integer> answer = new ArrayList<>();
+//         answer.add(arr[0]);
+        
+//         for (int i = 0; i < arr.length; i++) {
+//             if (answer.get(answer.size() - 1) != arr[i]) {
+//                 answer.add(arr[i]);
+//             }
+//         }
+
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 public class Solution {
-    public List solution(int []arr) {
-        List<Integer> answer = new ArrayList<>();
-        answer.add(arr[0]);
+    public int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<>();
+        int preNum = 10;
         
-        for (int i = 0; i < arr.length; i++) {
-            if (answer.get(answer.size() - 1) != arr[i]) {
-                answer.add(arr[i]);
+        for (int num : arr) {
+            if (preNum != num) {
+                list.add(num);
             }
+            preNum = num;
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
 
         return answer;
