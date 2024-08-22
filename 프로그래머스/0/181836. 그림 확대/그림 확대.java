04b623b4fa77ca5ef7563fbdc51ffabc3b6111e9
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-22 11:12:26 +0900

    [level 0] Title: 그림 확대, Time: 2.76 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,23 +1,45 @@
+// import java.util.*;
+
+// class Solution {
+//     public String[] solution(String[] picture, int k) {
+//         String[] temp = new String[picture.length];
+        
+//         for (int i = 0; i < picture.length; i++) {
+//             temp[i] = "";
+//             for (int j = 0; j < picture[i].length(); j++) {
+//                 for (int l = 0; l < k; l++) {
+//                     temp[i] += picture[i].charAt(j);
+//                 }
+//             }
+//         }
+        
+//         String[] answer = new String[picture.length * k];
+        
+//         for (int i = 0; i < answer.length; i++) {
+//             for (int j = 0; j < k; j++) {
+//                 answer[i] = temp[i / k];
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public String[] solution(String[] picture, int k) {
-        String[] temp = new String[picture.length];
+        String[] answer = new String[picture.length * k];
+        int idx = 0;
         
         for (int i = 0; i < picture.length; i++) {
-            temp[i] = "";
-            for (int j = 0; j < picture[i].length(); j++) {
-                for (int l = 0; l < k; l++) {
-                    temp[i] += picture[i].charAt(j);
-                }
-            }
-        }
-        
-        String[] answer = new String[picture.length * k];
-        
-        for (int i = 0; i < answer.length; i++) {
             for (int j = 0; j < k; j++) {
-                answer[i] = temp[i / k];
+                StringBuilder sb = new StringBuilder();
+                
+                for (int m = 0; m < picture[i].length(); m++) {
+                    sb.append(String.valueOf(picture[i].charAt(m)).repeat(k));
+                }
+                answer[idx++] = sb.toString();
             }
         }
         
