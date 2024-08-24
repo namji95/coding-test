d15ffe66781680d4c56a6c544445afc178e982dd
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-24 14:51:50 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.15 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,20 +1,34 @@
+// class Solution {
+//     public long solution(String numbers) {
+//         long answer = 0;
+//         String[] num = {
+//             "zero", "one", "two", "three", "four", 
+//             "five", "six", "seven", "eight", "nine"};
+//         StringBuilder result = new StringBuilder();
+        
+//         while (!numbers.isEmpty()) {
+//             for (int i = 0; i < num.length; i++) {
+//                 if (numbers.startsWith(num[i])) {
+//                     result.append(i);
+//                     numbers = numbers.substring(num[i].length());
+//                     break;
+//                 }
+//             }
+//         }
+//         return Long.parseLong(result.toString());
+//     }
+// }
+
 class Solution {
     public long solution(String numbers) {
-        long answer = 0;
         String[] num = {
             "zero", "one", "two", "three", "four", 
             "five", "six", "seven", "eight", "nine"};
-        StringBuilder result = new StringBuilder();
         
-        while (!numbers.isEmpty()) {
-            for (int i = 0; i < num.length; i++) {
-                if (numbers.startsWith(num[i])) {
-                    result.append(i);
-                    numbers = numbers.substring(num[i].length());
-                    break;
-                }
-            }
+        for (int i = 0; i < num.length; i++) {
+            numbers = numbers.replace(num[i], String.valueOf(i));
         }
-        return Long.parseLong(result.toString());
+        
+        return Long.parseLong(numbers);
     }
 }
