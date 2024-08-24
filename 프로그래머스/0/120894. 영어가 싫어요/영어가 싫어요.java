e061a3e755d98905b8af05928671cc88c61f9d23
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-24 15:27:22 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.18 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,11 +1,9 @@
 class Solution {
     public long solution(String numbers) {
-        long answer = 0;
-        String[] num = {
-            "zero", "one", "two", "three", "four", 
-            "five", "six", "seven", "eight", "nine"};
+        String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         StringBuilder result = new StringBuilder();
-        
+        long answer = 0;
+
         while (!numbers.isEmpty()) {
             for (int i = 0; i < num.length; i++) {
                 if (numbers.startsWith(num[i])) {
@@ -15,38 +13,20 @@ class Solution {
                 }
             }
         }
+
+
         return Long.parseLong(result.toString());
     }
 }
 
 // class Solution {
 //     public long solution(String numbers) {
-//         String[] num = {
-//             "zero", "one", "two", "three", "four", 
-//             "five", "six", "seven", "eight", "nine"};
-        
+//         String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
 //         for (int i = 0; i < num.length; i++) {
 //             numbers = numbers.replace(num[i], String.valueOf(i));
 //         }
         
 //         return Long.parseLong(numbers);
-//     }
-// }
-
-// class Solution {
-//     public long solution(String numbers) {
-//         return Long.parseLong(
-//             numbers
-//                 .replace("zero", "0")
-//                 .replace("one", "1")
-//                 .replace("two", "2")
-//                 .replace("three", "3")
-//                 .replace("four", "4")
-//                 .replace("five", "5")
-//                 .replace("six", "6")
-//                 .replace("seven", "7")
-//                 .replace("eight", "8")
-//                 .replace("nine", "9")
-//         );
 //     }
 // }
