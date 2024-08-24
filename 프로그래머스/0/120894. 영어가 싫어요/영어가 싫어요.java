dbb0ae3a55410f3c8dbc14a6e4f5a8d86f109d0e
NAMJI <138655542+namji95@users.noreply.github.com>
2024-08-24 15:30:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.23 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -30,3 +30,22 @@ class Solution {
 //         return Long.parseLong(numbers);
 //     }
 // }
+
+// class Solution {
+//     public long solution(String numbers) {
+        
+//         return Long.parseLong(
+//             numbers
+//                 .replace("zero", "0")
+//                 .replace("one", "1")
+//                 .replace("two", "2")
+//                 .replace("three", "3")
+//                 .replace("four", "4")
+//                 .replace("five", "5")
+//                 .replace("six", "6")
+//                 .replace("seven", "7")
+//                 .replace("eight", "8")
+//                 .replace("nine", "9")
+//        );
+//     }
+// }
