00f8c4a9b1285366331db3287a50aaa408ffe5e8
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-01 16:29:26 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 27.78 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,24 +1,46 @@
+// class Solution {
+//     public StringBuilder solution(String my_string, int[][] queries) {
+//         StringBuilder answer = new StringBuilder(my_string);
+        
+//         for (int i = 0; i < queries.length; i++) {
+//             StringBuilder front = new StringBuilder();
+//             StringBuilder back = new StringBuilder();
+//             StringBuilder reverse = new StringBuilder();
+//             if (queries[i][0] > 0) {
+//                 front.append(answer.substring(0, queries[i][0]));
+//             }
+//             if (queries[i][1] < my_string.length()) {
+//                 back.append(answer.substring(Math.min(queries[i][1] + 1, my_string.length())));
+//             }
+//             reverse.append(answer.substring(queries[i][0], Math.min(queries[i][1] + 1, answer.length())));
+//             reverse.reverse();
+//             answer.setLength(0);
+//             answer
+//                 .append(front)
+//                 .append(reverse)
+//                 .append(back);
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
-    public StringBuilder solution(String my_string, int[][] queries) {
-        StringBuilder answer = new StringBuilder(my_string);
+    public String solution(String my_string, int[][] queries) {
+        String answer = my_string;
         
         for (int i = 0; i < queries.length; i++) {
-            StringBuilder front = new StringBuilder();
-            StringBuilder back = new StringBuilder();
-            StringBuilder reverse = new StringBuilder();
-            if (queries[i][0] > 0) {
-                front.append(answer.substring(0, queries[i][0]));
-            }
-            if (queries[i][1] < my_string.length()) {
-                back.append(answer.substring(Math.min(queries[i][1] + 1, my_string.length())));
-            }
-            reverse.append(answer.substring(queries[i][0], Math.min(queries[i][1] + 1, answer.length())));
+            int s = queries[i][0];
+            int e = queries[i][1];
+            
+            StringBuilder reverse = new StringBuilder(answer.substring(s, e + 1));
             reverse.reverse();
-            answer.setLength(0);
-            answer
-                .append(front)
-                .append(reverse)
-                .append(back);
+            
+            if (s == 0) {
+                answer = reverse + answer.substring(e + 1);
+            } else {
+                answer = answer.substring(0, s) + reverse + answer.substring(e + 1);
+            }
         }
         
         return answer;
