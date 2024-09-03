7a1ba6addac54b972a9dc6d22cdf439af0f629d4
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-03 22:29:02 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[2];
+        
+        for (int i = 0; i < keyinput.length; i++) {
+            switch (keyinput[i]) {
+                case "left":
+                    if (-((board[0] - 1) / 2) < answer[0]) {
+                        answer[0]--;
+                    }
+                    break;
+                case "right":
+                    if (((board[0] - 1) / 2) > answer[0]) {
+                        answer[0]++;
+                    }
+                    break;
+                case "down":
+                    if (-((board[1] - 1) / 2) < answer[1]) {
+                        answer[1]--;
+                    }
+                    break;
+                case "up":
+                    if (((board[1] - 1) / 2) > answer[1]) {
+                        answer[1]++;
+                    }
+                    break;
+                }
+            }
+
+        return answer;
+    }
+}
