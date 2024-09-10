fcdad21d90815577d5a2cd666aa12f6829bde3be
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-10 13:30:16 +0900

    [level 0] Title: 등수 매기기, Time: 0.75 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -1,22 +1,42 @@
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[][] score) {
+//         double[] avg = new double[score.length];
+//         int[] answer = new int[score.length];
+
+//         for (int i = 0; i < score.length; i++) {
+//             avg[i] = (double) (score[i][0] + score[i][1]) / 2.0;
+//         }
+
+//         for (int i = 0; i < avg.length; i++) {
+//             int rank = 1;
+//             for (int j = 0; j < avg.length; j++) {
+//                 if (avg[i] < avg[j]) {
+//                     rank++;
+//                 }
+//             }
+//             answer[i] = rank;
+//         }
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int[][] score) {
-        double[] avg = new double[score.length];
-        int[] answer = new int[score.length];
-
-        for (int i = 0; i < score.length; i++) {
-            avg[i] = (double) (score[i][0] + score[i][1]) / 2.0;
+        List<Integer> scoreList = new ArrayList<>();
+        
+        for (int[] t : score) {
+            scoreList.add(t[0] + t[1]);
         }
+        scoreList.sort(Comparator.reverseOrder());
 
-        for (int i = 0; i < avg.length; i++) {
-            int rank = 1;
-            for (int j = 0; j < avg.length; j++) {
-                if (avg[i] < avg[j]) {
-                    rank++;
-                }
-            }
-            answer[i] = rank;
+        int[] answer = new int[score.length];
+        for (int i = 0; i < score.length; i++) {
+            answer[i] = scoreList.indexOf((score[i][0] + score[i][1])) + 1;
         }
         
         return answer;
