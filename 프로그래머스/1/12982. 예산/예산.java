277dc7c14832f1e409bf85d89631764ccbe597ef
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-11 22:15:12 +0900

    [level 1] Title: 예산, Time: 0.58 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -21,6 +21,25 @@
 //     }
 // }
 
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[] d, int budget) {
+//         int answer = 0;
+//         Arrays.sort(d);
+        
+//         for (int i = 0; i < d.length; i++) {
+//             budget -= d[i];
+//             if (budget < 0) {
+//                 break;
+//             }
+//             answer++;
+//         }
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
@@ -28,13 +47,7 @@ class Solution {
         int answer = 0;
         Arrays.sort(d);
         
-        for (int i = 0; i < d.length; i++) {
-            budget -= d[i];
-            if (budget < 0) {
-                break;
-            }
-            answer++;
-        }
+        while((budget -= d[answer]) >= 0 && ++answer < d.length);
         
         return answer;
     }
