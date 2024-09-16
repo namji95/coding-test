b70cce8889a80002c1030c838fb87f8e159c5834
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-16 10:07:48 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 3.15 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -25,9 +25,31 @@
 //     }
 // }
 
+// class Solution {
+//     public String solution(String my_string, int[][] queries) {
+//         String answer = my_string;
+        
+//         for (int i = 0; i < queries.length; i++) {
+//             int s = queries[i][0];
+//             int e = queries[i][1];
+            
+//             StringBuilder reverse = new StringBuilder(answer.substring(s, e + 1));
+//             reverse.reverse();
+            
+//             if (s == 0) {
+//                 answer = reverse + answer.substring(e + 1);
+//             } else {
+//                 answer = answer.substring(0, s) + reverse + answer.substring(e + 1);
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
     public String solution(String my_string, int[][] queries) {
-        String answer = my_string;
+        StringBuilder answer = new StringBuilder(my_string);
         
         for (int i = 0; i < queries.length; i++) {
             int s = queries[i][0];
@@ -36,13 +58,9 @@ class Solution {
             StringBuilder reverse = new StringBuilder(answer.substring(s, e + 1));
             reverse.reverse();
             
-            if (s == 0) {
-                answer = reverse + answer.substring(e + 1);
-            } else {
-                answer = answer.substring(0, s) + reverse + answer.substring(e + 1);
-            }
+            answer.replace(s, e + 1, reverse.toString());
         }
         
-        return answer;
+        return answer.toString(); // 최종 결과를 String으로 반환
     }
 }
