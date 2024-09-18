12b12ffe1383334bfe30d1fff88be8d548a307d4
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-18 19:35:46 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,17 +1,25 @@
+// class Solution {
+//     public int solution(String A, String B) {
+//         int answer = 0;
+        
+//         for (int i = 0; i < A.length(); i++) {
+//             String newStr = A.substring(A.length() - i) + A.substring(0, A.length() - i);
+//             if (B.equals(newStr)) {
+//                 answer = i;
+//                 break;
+//             } else {
+//                 answer = -1;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
+        String tempB = B.repeat(2);
         
-        for (int i = 0; i < A.length(); i++) {
-            String newStr = A.substring(A.length() - i) + A.substring(0, A.length() - i);
-            if (B.equals(newStr)) {
-                answer = i;
-                break;
-            } else {
-                answer = -1;
-            }
-        }
-        
-        return answer;
+        return tempB.indexOf(A);
     }
 }
