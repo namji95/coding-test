3d12f177fae4c030894587164984c1ef52d5d25b
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-18 18:25:08 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.52 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,19 +1,39 @@
+// import java.util.*;
+
+// class Solution {
+//     public List solution(int l, int r) {
+//         List<Integer> answer = new ArrayList<>();
+        
+//         for (int i = l; i <= r; i++) {
+//             String s = i + "";
+//             boolean bool = true;
+//             for (char c : s.toCharArray()) {
+//                 if (c != '0' && c != '5') {
+//                     bool = false;
+//                 }
+//             }
+//             if (bool) {
+//                 answer.add(i);
+//             }
+//         }
+//         if (answer.isEmpty()) {
+//             answer.add(-1);
+//         }
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public List solution(int l, int r) {
         List<Integer> answer = new ArrayList<>();
         
-        for (int i = l; i <= r; i++) {
-            String s = i + "";
-            boolean bool = true;
-            for (char c : s.toCharArray()) {
-                if (c != '0' && c != '5') {
-                    bool = false;
-                }
-            }
-            if (bool) {
-                answer.add(i);
+        for (int i = 0; i < 64; i++) {
+            int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
+            if (l <= num && num <= r) {
+                answer.add(num);
             }
         }
         if (answer.isEmpty()) {
