47e4a86609c93e6500f8561c67de37379b6800f8
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-19 13:45:16 +0900

    [level 1] Title: 예산, Time: 0.55 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,25 +1,25 @@
-// import java.util.*;
+import java.util.*;
 
-// class Solution {
-//     public int solution(int[] d, int budget) {
-//         int sum = 0;
-//         int answer = 0;
-//         Arrays.sort(d);
+class Solution {
+    public int solution(int[] d, int budget) {
+        int sum = 0;
+        int answer = 0;
+        Arrays.sort(d);
         
-//         for (int i = 0; i < d.length; i++) {
-//             if (sum <= budget) {
-//                 sum += d[i];
-//                 answer++;
-//             }
-//             if (sum > budget) {
-//                 sum -= d[i];
-//                 answer--;
-//             }
-//         }
+        for (int i = 0; i < d.length; i++) {
+            if (sum <= budget) {
+                sum += d[i];
+                answer++;
+            }
+            if (sum > budget) {
+                sum -= d[i];
+                answer--;
+            }
+        }
         
-//         return answer;
-//     }
-// }
+        return answer;
+    }
+}
 
 // import java.util.*;
 
@@ -40,15 +40,15 @@
 //     }
 // }
 
-import java.util.*;
+// import java.util.*;
 
-class Solution {
-    public int solution(int[] d, int budget) {
-        int answer = 0;
-        Arrays.sort(d);
+// class Solution {
+//     public int solution(int[] d, int budget) {
+//         int answer = 0;
+//         Arrays.sort(d);
         
-        while((budget -= d[answer]) >= 0 && ++answer < d.length);
+//         while((budget -= d[answer]) >= 0 && ++answer < d.length);
         
-        return answer;
-    }
-}
+//         return answer;
+//     }
+// }
