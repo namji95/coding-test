9882b6837e55b870bb654766da1c382b26e53e26
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-19 11:35:08 +0900

    [level 0] Title: 배열 조각하기, Time: 0.05 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -1,17 +1,38 @@
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] arr, int[] query) {
+//         int[] answer = {};
+        
+//         for (int i = 0; i < query.length; i++) {
+//             if (i % 2 == 0) {
+//                 arr = Arrays.copyOfRange(arr, 0, query[i] + 1);
+//             } else {
+//                 arr = Arrays.copyOfRange(arr, query[i], arr.length);
+//             }
+//         }
+//         answer = Arrays.copyOf(arr, arr.length);
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
         int[] answer = {};
+        int start = 0;
+        int end = arr.length - 1;
         
         for (int i = 0; i < query.length; i++) {
             if (i % 2 == 0) {
-                arr = Arrays.copyOfRange(arr, 0, query[i] + 1);
+                end = start + query[i] - 1;
             } else {
-                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+                start += query[i];
             }
         }
-        answer = Arrays.copyOf(arr, arr.length);
+        answer = Arrays.copyOfRange(arr, start, end + 2);
         
         return answer;
     }
