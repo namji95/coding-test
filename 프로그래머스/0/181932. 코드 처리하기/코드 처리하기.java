828848ea46c95e71bb1861663371bb56c861ce58
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-20 11:12:36 +0900

    [level 0] Title: 코드 처리하기, Time: 10.96 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,23 +1,44 @@
+// class Solution {
+//     public StringBuilder solution(String code) {
+//         int mode = 0;
+//         StringBuilder answer = new StringBuilder();
+        
+//         for (int i = 0; i < code.length(); i++) {
+//             if (code.charAt(i) == '1') {
+//                 mode = mode == 0 ? 1 : 0;
+//                 continue;
+//             }
+//             if (mode == 0 && i % 2 == 0) {
+//                 answer.append(code.charAt(i));
+//             } else if (mode == 1 && i % 2 != 0) {
+//                 answer.append(code.charAt(i));
+//             }
+//         }
+//         if (answer.length() == 0) {
+//             answer.append("EMPTY");
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
-    public StringBuilder solution(String code) {
+    public String solution(String code) {
         int mode = 0;
         StringBuilder answer = new StringBuilder();
         
         for (int i = 0; i < code.length(); i++) {
-            if (code.charAt(i) == '1') {
+            char c = code.charAt(i);
+            if (c == '1') {
                 mode = mode == 0 ? 1 : 0;
                 continue;
             }
-            if (mode == 0 && i % 2 == 0) {
-                answer.append(code.charAt(i));
-            } else if (mode == 1 && i % 2 != 0) {
-                answer.append(code.charAt(i));
+            
+            if (i % 2 == mode) {
+                answer.append(c);
             }
         }
-        if (answer.length() == 0) {
-            answer.append("EMPTY");
-        }
         
-        return answer;
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
     }
 }
