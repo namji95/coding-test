53e49c07624ab6f8bd340e70643642387884f55f
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-24 13:03:37 +0900

    [level 0] Title: OX퀴즈, Time: 0.22 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -1,20 +1,34 @@
+// class Solution {
+//     public String[] solution(String[] quiz) {
+//         String[] answer = new String[quiz.length];
+        
+//         for (int i = 0; i < quiz.length; i++) {
+//             String[] str = quiz[i].split(" ");
+//             int first = Integer.parseInt(str[0]);
+//             int second = Integer.parseInt(str[2]);
+//             int third = Integer.parseInt(str[4]);
+//             if ((quiz[i].contains(" + ") && first + second == third) ||
+//                     (quiz[i].contains(" - ") && first - second == third)) {
+//                 answer[i] = "O";
+//             } else {
+//                 answer[i] = "X";
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
     public String[] solution(String[] quiz) {
-        String[] answer = new String[quiz.length];
         
-        for (int i = 0; i < quiz.length; i++) {
-            String[] str = quiz[i].split(" ");
-            int first = Integer.parseInt(str[0]);
-            int second = Integer.parseInt(str[2]);
-            int third = Integer.parseInt(str[4]);
-            if ((quiz[i].contains(" + ") && first + second == third) ||
-                    (quiz[i].contains(" - ") && first - second == third)) {
-                answer[i] = "O";
-            } else {
-                answer[i] = "X";
-            }
+        for(int i=0; i<quiz.length; i++){
+            String[] text = quiz[i].split(" ");
+            int result = Integer.parseInt(text[0]) +
+                    (Integer.parseInt(text[2]) * (text[1].equals("+") ? 1:-1));
+            quiz[i] = result == Integer.parseInt(text[4])? "O": "X";
         }
         
-        return answer;
+        return quiz;
     }
 }
