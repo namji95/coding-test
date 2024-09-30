eafa882bb6d64b96596cdc95f4e54ef70f7cb28b
NAMJI <138655542+namji95@users.noreply.github.com>
2024-09-30 16:17:47 +0900

    [level 0] Title: 주사위 게임 3, Time: 1.30 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int[] dice = {a, b, c, d};
+        Arrays.sort(dice);
+        int answer = 0;
+
+        if (dice[0] == dice[1] && dice[1] == dice[2] && dice[2] == dice[3]) {
+            answer = 1111 * dice[0];
+        } else if (dice[0] == dice[1] && dice[1] == dice[2]) {
+            answer = (int) Math.pow((10 * dice[0] + dice[3]), 2);
+        } else if (dice[1] == dice[2] && dice[2] == dice[3]) {
+            answer = (int) Math.pow((10 * dice[1] + dice[0]), 2);
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            answer = (dice[0] + dice[3]) * (Math.abs(dice[0] - dice[3]));
+        } else if (dice[0] == dice[1]) {
+            answer = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            answer = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            answer = dice[0] * dice[1];
+        } else {
+            answer = dice[0];
+        }
+        return answer;
+    }
+}
