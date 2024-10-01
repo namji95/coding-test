4578a815983e878c0c63d3e69bcfb574750b74c9
NAMJI <138655542+namji95@users.noreply.github.com>
2024-10-01 17:42:34 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -3,10 +3,9 @@ class Solution {
         int answer = 0;
         
         if (common[1] - common[0] == common[2] - common[1]) {
-            answer = common[common.length - 1] + common[1] - common[0];
+            answer = common[common.length - 1] + (common[1] - common[0]);
         } else {
-            int quotient = common[1] / common[0];
-            answer = common[common.length - 1] * quotient;
+            answer = common[common.length - 1] * (common[1] / common[0]);
         }
         
         return answer;
