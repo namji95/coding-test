a275049a2351cef740505f634050830703fcfeff
NAMJI <138655542+namji95@users.noreply.github.com>
2024-10-02 15:09:52 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.24 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -85,23 +85,30 @@
 //     }
 // }
 
+// class Solution {
+//     public int solution(int a, int b) {
+//         int answer = 0;
+
+//         for (;b%2 == 0;) {
+//             b = b/2;
+//         }
+//         for (;b%5 == 0;) {
+//             b = b/5;
+//         }
+
+//         if ((a/(double)b)%1 == 0) {
+//             answer = 1;
+//         } else {
+//             answer = 2;
+//         }
+
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-
-        for (;b%2 == 0;) {
-            b = b/2;
-        }
-        for (;b%5 == 0;) {
-            b = b/5;
-        }
-
-        if ((a/(double)b)%1 == 0) {
-            answer = 1;
-        } else {
-            answer = 2;
-        }
-
+        int answer = ((a*320000)%b == 0) ? 1 : 2;
         return answer;
     }
 }
