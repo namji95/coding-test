d370bc4d0dff14fec3206ee952b8d775656f5433
NAMJI <138655542+namji95@users.noreply.github.com>
2024-10-02 15:08:37 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -60,27 +60,48 @@
 //     }
 // }
 
+// class Solution {
+//     public int solution(int a, int b) {
+//         b /= gcd(a, b);
+//         while (b != 1) {
+//             if (b % 5 == 0) {
+//                 b /= 5;
+//                 continue;
+//             }
+//             if (b % 2 == 0) {
+//                 b /= 2;
+//                 continue;
+//             }
+//             return 2;
+//         }
+//         return 1;
+//     }
+
+//     private int gcd(int a, int b) {
+//         if (b == 0) {
+//             return a;
+//         }
+//         return gcd(b, a % b);
+//     }
+// }
+
 class Solution {
     public int solution(int a, int b) {
-        b /= gcd(a, b);
-        while (b != 1) {
-            if (b % 5 == 0) {
-                b /= 5;
-                continue;
-            }
-            if (b % 2 == 0) {
-                b /= 2;
-                continue;
-            }
-            return 2;
-        }
-        return 1;
-    }
+        int answer = 0;
 
-    private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
+        for (;b%2 == 0;) {
+            b = b/2;
         }
-        return gcd(b, a % b);
+        for (;b%5 == 0;) {
+            b = b/5;
+        }
+
+        if ((a/(double)b)%1 == 0) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+
+        return answer;
     }
 }
