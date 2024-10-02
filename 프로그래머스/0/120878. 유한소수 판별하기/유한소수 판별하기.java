484f29a0f3d98d3ec0283386c729935624e2da10
NAMJI <138655542+namji95@users.noreply.github.com>
2024-10-02 15:06:58 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,61 +1,86 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int a, int b) {
+        
+//         int fraction = fraction(a, b);
+//         List<Integer> primeFactors = getPrimeFactors(fraction);
+        
+//         return checkDecimalType(primeFactors);
+//     }
+    
+//     // 최대공약수
+//     private static int gcd(int a, int b) {
+//         while (b != 0) {
+//             int temp = b;
+//             b = a % b;
+//             a = temp;
+//         }
+//         return a;
+//     }
+
+//     // 기약분수에서 분모만 반환
+//     private static int fraction(int a, int b) {
+//         int numerator = a;
+//         int denominator = b;
+
+//         int gcdValue = gcd(numerator, denominator);
+
+//         // 분자, 분모 기약분수 만들기
+//         denominator /= gcdValue;
+
+//         return denominator;
+//     }
+
+//     // 소인수
+//     private static List<Integer> getPrimeFactors(int fraction) {
+//         List<Integer> primeFactors = new ArrayList<>();
+
+//         // 2로 나누어 떨어질 때까지 나누기
+//         for (int i = 2; i <= fraction; i++) {
+//             while (fraction % i == 0) {
+//                 primeFactors.add(i);
+//                 fraction /= i;
+//             }
+//         }
+//         return primeFactors;
+//     }
+
+//     // 무한소수 유한소수 판별
+//     private static int checkDecimalType(List<Integer> primeFactors) {
+//         int answer = 1;
+//         for (int i : primeFactors) {
+//             if (i != 2 && i != 5) {
+//                 answer = 2;
+//                 break;
+//             }
+//         }
+
+//         return answer;
+//     }
+// }
 
 class Solution {
     public int solution(int a, int b) {
-        
-        int fraction = fraction(a, b);
-        List<Integer> primeFactors = getPrimeFactors(fraction);
-        
-        return checkDecimalType(primeFactors);
-    }
-    
-    // 최대공약수
-    private static int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-
-    // 기약분수에서 분모만 반환
-    private static int fraction(int a, int b) {
-        int numerator = a;
-        int denominator = b;
-
-        int gcdValue = gcd(numerator, denominator);
-
-        // 분자, 분모 기약분수 만들기
-        denominator /= gcdValue;
-
-        return denominator;
-    }
-
-    // 소인수
-    private static List<Integer> getPrimeFactors(int fraction) {
-        List<Integer> primeFactors = new ArrayList<>();
-
-        // 2로 나누어 떨어질 때까지 나누기
-        for (int i = 2; i <= fraction; i++) {
-            while (fraction % i == 0) {
-                primeFactors.add(i);
-                fraction /= i;
+        b /= gcd(a, b);
+        while (b != 1) {
+            if (b % 5 == 0) {
+                b /= 5;
+                continue;
             }
+            if (b % 2 == 0) {
+                b /= 2;
+                continue;
+            }
+            return 2;
         }
-        return primeFactors;
+        return 1;
     }
 
-    // 무한소수 유한소수 판별
-    private static int checkDecimalType(List<Integer> primeFactors) {
-        int answer = 1;
-        for (int i : primeFactors) {
-            if (i != 2 && i != 5) {
-                answer = 2;
-                break;
-            }
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
         }
-
-        return answer;
+        return gcd(b, a % b);
     }
 }
