eb76df278e014a7abb99006c30502c0e3153061b
NAMJI <138655542+namji95@users.noreply.github.com>
2024-10-09 20:07:46 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.51 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,62 +1,17 @@
-// import java.util.*;
-
-// class Solution {
-//     public int solution(int[][] lines) {
-//         HashMap<Integer, Integer> line = new HashMap<>();
-//         int answer = 0;
-        
-//         for (int[] i : lines) {
-//             for (int j = i[0]; j < i[1]; j++) {
-//                 line.put(j, line.getOrDefault(j, 0) + 1);
-//             }
-//         }
-//         for (int val : line.values()) {
-//             if (val > 1) {
-//                 answer++;
-//             }
-//         }
-        
-//         return answer;
-//     }
-// }
-
-// import java.util.*;
-
-// class Solution {
-//     public int solution(int[][] lines) {
-//         HashMap<Integer, Integer> line = new HashMap<>();
-//         int answer = 0;
-        
-//         for (int[] i : lines) {
-//             for (int j = i[0]; j < i[1]; j++) {
-//                 line.merge(j, 1, Integer::sum);
-//             }
-//         }
-        
-//         answer = (int) line.values().stream().filter(i -> i > 1).count();
-        
-//         return answer;
-//     }
-// }
-
 import java.util.*;
 
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
-        
-        for (int i = -100; i < 100; i++) {
-            int line = 0;
-            if (lines[0][0] <= i && lines[0][1] > i) {
-                line++;
+        HashMap<Integer, Integer> line = new HashMap<>();
+
+        for (int[] i : lines) {
+            for (int j = i[0]; j < i[1]; j++) {
+                line.put(j, line.getOrDefault(j, 0) + 1);
             }
-            if (lines[1][0] <= i && lines[1][1] > i) {
-                line++;
-            }
-            if (lines[2][0] <= i && lines[2][1] > i) {
-                line++;
-            }
-            if (line > 1) {
+        }
+        for (int val : line.values()) {
+            if (val > 1) {
                 answer++;
             }
         }
@@ -64,3 +19,50 @@ class Solution {
         return answer;
     }
 }
+
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[][] lines) {
+//         int answer = 0;
+//         HashMap<Integer, Integer> line = new HashMap<>();
+
+//         for (int[] i : lines) {
+//             int from = i[0];
+//             int to = i[1];
+//             for (int j = from; j < to; j++) {
+//                 line.merge(j, 1, Integer::sum);
+//             }
+//         }
+
+//         answer = (int) line.values().stream().filter(i -> i > 1).count();
+        
+//         return answer;
+//     }
+// }
+
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[][] lines) {
+//         int answer = 0;
+        
+//         for (int i = -100; i < 100; i++) {
+//             int line = 0;
+//             if (lines[0][0] <= i && lines[0][1] > i) {
+//                 line++;
+//             }
+//             if (lines[1][0] <= i && lines[1][1] > i) {
+//                 line++;
+//             }
+//             if (lines[2][0] <= i && lines[2][1] > i) {
+//                 line++;
+//             }
+//             if (line > 1) {
+//                 answer++;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
