e42d8a51dbe3df7619ae099a45232bb2ea8a6109
NAMJI <138655542+namji95@users.noreply.github.com>
2024-10-09 18:14:02 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 2.47 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,3 +1,25 @@
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[][] lines) {
+//         HashMap<Integer, Integer> line = new HashMap<>();
+//         int answer = 0;
+        
+//         for (int[] i : lines) {
+//             for (int j = i[0]; j < i[1]; j++) {
+//                 line.put(j, line.getOrDefault(j, 0) + 1);
+//             }
+//         }
+//         for (int val : line.values()) {
+//             if (val > 1) {
+//                 answer++;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
@@ -7,14 +29,11 @@ class Solution {
         
         for (int[] i : lines) {
             for (int j = i[0]; j < i[1]; j++) {
-                line.put(j, line.getOrDefault(j, 0) + 1);
+                line.merge(j, 1, Integer::sum);
             }
         }
-        for (int val : line.values()) {
-            if (val > 1) {
-                answer++;
-            }
-        }
+        
+        answer = (int) line.values().stream().filter(i -> i > 1).count();
         
         return answer;
     }
