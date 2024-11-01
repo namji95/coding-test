81d22f847736c87e23ac8bf4193bb28771ae5e4a
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-01 16:51:12 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.26 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,18 +1,34 @@
+// import java.util.*;
+
+// class Solution {
+//     public List solution(int[] numbers) {
+//         List<Integer> answer = new ArrayList<>();
+
+//         for (int i = 0; i < numbers.length; i++) {
+//             for (int j = 0; j < numbers.length; j++) {
+//                 int plus = numbers[i] + numbers[j];
+//                 if (i != j && !answer.contains(plus)) {
+//                     answer.add(plus);
+//                 }
+//             }
+//         }
+//         Collections.sort(answer);
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
-    public List solution(int[] numbers) {
-        List<Integer> answer = new ArrayList<>();
+    public Set solution(int[] numbers) {
+        Set<Integer> answer = new TreeSet<>();
 
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers.length; j++) {
-                int plus = numbers[i] + numbers[j];
-                if (i != j && !answer.contains(plus)) {
-                    answer.add(plus);
-                }
+            for (int j = i+1; j < numbers.length; j++) {
+                answer.add(numbers[i] + numbers[j]);
             }
         }
-        Collections.sort(answer);
         
         return answer;
     }
