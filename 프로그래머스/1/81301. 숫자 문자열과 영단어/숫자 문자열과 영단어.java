4a72a1e57cc74ef8d3629eb238dcc85ea6da12e2
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-07 20:46:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -10,13 +10,35 @@
 //     }
 // }
 
+// class Solution {
+//     public int solution(String s) {
+//         String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+//         String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+//         for(int i=0; i<10; i++){
+//             s = s.replaceAll(alphabets[i],digits[i]);
+//         }
+        
+//         return Integer.parseInt(s);
+//     }
+// }
+
 class Solution {
     public int solution(String s) {
-        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
-        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        String[][] mapArr = {
+                {"0", "zero"},
+                {"1", "one"},
+                {"2", "two"},
+                {"3", "three"},
+                {"4", "four"},
+                {"5", "five"},
+                {"6", "six"},
+                {"7", "seven"},
+                {"8", "eight"},
+                {"9", "nine"} };
 
-        for(int i=0; i<10; i++){
-            s = s.replaceAll(alphabets[i],digits[i]);
+        for(String[] map : mapArr){
+            s = s.replace(map[1], map[0]);
         }
         
         return Integer.parseInt(s);
