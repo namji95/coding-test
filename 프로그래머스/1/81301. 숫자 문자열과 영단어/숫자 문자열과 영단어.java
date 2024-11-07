b5512a5876c2dd92c229b10affc20bd744ec04ef
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-07 20:45:18 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.53 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,9 +1,22 @@
+// class Solution {
+//     public int solution(String s) {
+//         String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+//         for (int i = 0; i < numbers.length; i++) {
+//             s = s.replaceAll(numbers[i], String.valueOf(i));
+//         }
+        
+//         return Integer.parseInt(s);
+//     }
+// }
+
 class Solution {
     public int solution(String s) {
-        String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        
-        for (int i = 0; i < numbers.length; i++) {
-            s = s.replaceAll(numbers[i], String.valueOf(i));
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        for(int i=0; i<10; i++){
+            s = s.replaceAll(alphabets[i],digits[i]);
         }
         
         return Integer.parseInt(s);
