8bc2b8df91288b414de74b74f026d656d7cca8b7
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-11 12:55:52 +0900

    [level 0] Title: k의 개수, Time: 24.13 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,15 +1,27 @@
+// class Solution {
+//     public int solution(int i, int j, int k) {
+//         int answer = 0;
+        
+//         for (int n = i; n <= j; n++) {
+//             for (char c : (n+"").toCharArray()) {
+//                 if (Integer.parseInt(String.valueOf(c)) == k) {
+//                     answer++;
+//                 }
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int solution(int i, int j, int k) {
-        int answer = 0;
+        StringBuilder str = new StringBuilder();
         
         for (int n = i; n <= j; n++) {
-            for (char c : (n+"").toCharArray()) {
-                if (Integer.parseInt(String.valueOf(c)) == k) {
-                    answer++;
-                }
-            }
+            str.append(n);
         }
         
-        return answer;
+        return str.length() - str.toString().replace(k+"", "").length();
     }
 }
