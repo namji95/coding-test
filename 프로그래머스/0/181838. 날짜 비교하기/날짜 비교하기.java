3bec3cc12fc85fb7be16f039533f779d6c0d2836
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-12 13:00:27 +0900

    [level 0] Title: 날짜 비교하기, Time: 1.17 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,17 +1,35 @@
+// class Solution {
+//     public int solution(int[] date1, int[] date2) {
+//         if (date2[0] > date1[0]) {
+//             return  1;
+//         } else if (date2[0] < date1[0]) {
+//             return 0;
+//         } else if (date2[1] > date1[1]) {
+//             return  1;
+//         } else if (date2[1] < date1[1]) {
+//             return 0;
+//         } else if (date2[2] > date1[2]) {
+//             return  1;
+//         }
+
+//         return 0;
+//     }
+// }
+
+import java.util.*;
+import java.time.*;
+
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        if (date2[0] > date1[0]) {
-            return  1;
-        } else if (date2[0] < date1[0]) {
-            return 0;
-        } else if (date2[1] > date1[1]) {
-            return  1;
-        } else if (date2[1] < date1[1]) {
-            return 0;
-        } else if (date2[2] > date1[2]) {
-            return  1;
-        }
+        int answer = 0;
 
-        return 0;
+        LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
+
+        if (dateA.isBefore(dateB)) {
+            answer = 1;
+        }
+        
+        return answer;
     }
 }
