d4ffab159fb85f6f185730635c5b1e41a8a8e755
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-14 12:18:23 +0900

    [level 0] Title: 문자열 묶기, Time: 6.46 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,17 +1,36 @@
+// import java.util.*;
+
+// class Solution {
+//     public int solution(String[] strArr) {
+//         int answer = 0;
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for (String str : strArr) {
+//             map.put(str.length(), map.getOrDefault(str.length(), 0) + 1);
+//         }
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             if (answer < entry.getValue()) {
+//                 answer = entry.getValue();
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public int solution(String[] strArr) {
         int answer = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (String str : strArr) {
-            map.put(str.length(), map.getOrDefault(str.length(), 0) + 1);
+        int[] lengArr = new int[31];
+        
+        for (int i = 0; i < strArr.length; i++) {
+            lengArr[strArr[i].length()]++;
         }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (answer < entry.getValue()) {
-                answer = entry.getValue();
-            }
+
+        for (int i = 0; i <= 30; i++) {
+            answer = Math.max(answer, lengArr[i]);
         }
         
         return answer;
