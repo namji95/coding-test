4579482ea1ecbe4a6fe50b8b57b4be0610371fc1
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-15 12:50:14 +0900

    [Bronze V] Title: 사분면 고르기, Time: 184 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -2,21 +2,39 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
         int x = in.nextInt();
         int y = in.nextInt();
 
-        solution1(x, y);
+//        solution1(x, y);
+        solution2(x, y);
     }
-    public static void solution1(int x, int y) {
-        if (x > 0 && y > 0) {
-            System.out.println(1);
-        } else if (x < 0 && y > 0) {
-            System.out.println(2);
-        } else if (x < 0 && y < 0) {
-            System.out.println(3);
+
+//    public static void solution1(int x, int y) {
+//        if (x > 0 && y > 0) {
+//            System.out.println(1);
+//        } else if (x < 0 && y > 0) {
+//            System.out.println(2);
+//        } else if (x < 0 && y < 0) {
+//            System.out.println(3);
+//        } else {
+//            System.out.println(4);
+//        }
+//    }
+
+    public static void solution2(int x, int y) {
+        if (x > 0) {
+            if (y > 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(4);
+            }
         } else {
-            System.out.println(4);
+            if (y > 0) {
+                System.out.println(2);
+            } else {
+                System.out.println(3);
+            }
         }
     }
 }
