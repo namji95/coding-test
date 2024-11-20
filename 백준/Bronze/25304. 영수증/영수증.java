41049f155f7d09a931f8c8eb870f0324262f8e30
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-20 12:51:42 +0900

    [Bronze IV] Title: 영수증, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,24 +1,41 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        solution1();
+    public static void main(String[] args) throws IOException {
+    //        solution1();
+        solution2();
     }
 
-    public static void solution1() {
-        Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        int n = in.nextInt();
+//    public static void solution1() {
+//        Scanner in = new Scanner(System.in);
+//        int x = in.nextInt();
+//        int n = in.nextInt();
+//        int result = 0;
+//        String answer = "";
+//
+//        for (int i = 0; i < n; i++) {
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            result += a * b;
+//        }
+//        answer = result == x ? "Yes" : "No";
+//
+//        System.out.println(answer);
+//    }
+
+    public static void solution2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int x = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         int result = 0;
-        String answer = "";
 
         for (int i = 0; i < n; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            result += a * b;
+            st = new StringTokenizer(br.readLine());
+            result += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
         }
-        answer = result == x ? "Yes" : "No";
 
-        System.out.println(answer);
+        System.out.println(result == x ? "Yes" : "No");
     }
 }
