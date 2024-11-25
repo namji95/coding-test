84dcba31f0d11f99e28931d659fb054dc3ef7239
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-25 12:38:49 +0900

    [Bronze V] Title: A+B - 5, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -4,7 +4,8 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 //        solution1();
-        solution2();
+//        solution2();
+        solution3();
     }
 
 //    public static void solution1() {
@@ -21,15 +22,34 @@ public class Main {
 //        }
 //    }
 
-    public static void solution2() throws IOException {
+//    public static void solution2() throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//        StringBuilder sb = new StringBuilder();
+//
+//        while(true) {
+//            st = new StringTokenizer(br.readLine(), " ");
+//            int A = Integer.parseInt(st.nextToken());
+//            int B = Integer.parseInt(st.nextToken());
+//
+//            if (A == 0 && B == 0) {
+//                break;
+//            }
+//
+//            sb.append(A + B).append("\n");
+//        }
+//
+//        System.out.println(sb);
+//    }
+
+    public static void solution3() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        while(true) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
+        while (true) {
+            String str = br.readLine();
+            int A = str.charAt(0) - 48;
+            int B = str.charAt(2) - 48;
 
             if (A == 0 && B == 0) {
                 break;
