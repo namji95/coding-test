70ef978b79c7456461feadba90c9df929db73e43
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-25 12:38:22 +0900

    [Bronze V] Title: A+B - 5, Time: 112 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,21 +1,43 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        solution1();
+    public static void main(String[] args) throws IOException {
+//        solution1();
+        solution2();
     }
 
-    public static void solution1() {
-        Scanner in = new Scanner(System.in);
+//    public static void solution1() {
+//        Scanner in = new Scanner(System.in);
+//
+//
+//        while (true) {
+//            int A = in.nextInt();
+//            int B = in.nextInt();
+//            if (A == 0 && B == 0) {
+//                break;
+//            }
+//            System.out.println(A + B);
+//        }
+//    }
 
+    public static void solution2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        while (true) {
-            int A = in.nextInt();
-            int B = in.nextInt();
+        while(true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
             if (A == 0 && B == 0) {
                 break;
             }
-            System.out.println(A + B);
+
+            sb.append(A + B).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
