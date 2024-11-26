d9d2c72b537ed9077644cb2913198aad831baaaf
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-26 12:29:33 +0900

    [Bronze V] Title: A+B - 4, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,18 +1,35 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        solution1();
+    public static void main(String[] args) throws IOException {
+//        solution1();
+        solution2();
     }
 
-    public static void solution1() {
-        Scanner in = new Scanner(System.in);
+//    public static void solution1() {
+//        Scanner in = new Scanner(System.in);
+//
+//        while (in.hasNextInt()) {
+//            int A = in.nextInt();
+//            int B = in.nextInt();
+//            System.out.println(A + B);
+//        }
+//        in.close();
+//    }
 
-        while (in.hasNextInt()) {
-            int A = in.nextInt();
-            int B = in.nextInt();
-            System.out.println(A + B);
+    public static void solution2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+
+        while ((str=br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
+            int A  = Integer.parseInt(st.nextToken());
+            int B  = Integer.parseInt(st.nextToken());
+            sb.append(A+B).append("\n");
         }
-        in.close();
+        System.out.println(sb);
     }
 }
