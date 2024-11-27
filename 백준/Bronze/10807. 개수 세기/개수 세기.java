d81b8fb8c3f21032e666351d93016509ab5cb6ab
NAMJI <138655542+namji95@users.noreply.github.com>
2024-11-27 12:32:19 +0900

    [Bronze V] Title: 개수 세기, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,27 +1,57 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        solution1();
+    public static void main(String[] args) throws IOException {
+//        solution1();
+        solution2();
     }
 
-    private static void solution1() {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int[] arr = new int[N];
+//    public static void solution1() {
+//        Scanner in = new Scanner(System.in);
+//        int N = in.nextInt();
+//        int[] arr = new int[N];
+//        int cnt = 0;
+//
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = in.nextInt();
+//        }
+//        int V = in.nextInt();
+//
+//        for (int i : arr) {
+//            if (i == V) {
+//                cnt++;
+//            }
+//        }
+//
+//        System.out.println(cnt);
+//        
+//        in.close();
+//    }
+    
+    public static void solution2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         int cnt = 0;
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
         for (int i = 0; i < N; i++) {
-            arr[i] = in.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int V = in.nextInt();
-
+        
+        int V = Integer.parseInt(br.readLine());
+        
         for (int i : arr) {
             if (i == V) {
                 cnt++;
             }
         }
-
         System.out.println(cnt);
+        
+        br.close();
+        bw.close();
+        bw.close();
     }
 }
