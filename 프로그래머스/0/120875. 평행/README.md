1c818901ecb7c144481a47d9a640501c463cff35
NAMJI <138655542+namji95@users.noreply.github.com>
2024-12-01 18:20:55 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/README.md
+++ 프로그래머스/0/120875. 평행/README.md
@@ -1,10 +1,10 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 0.03 ms
+메모리: 74.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 17일 12:46:31
+2024년 12월 01일 18:20:52
 
 ### 문제 설명
 
--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,33 +1,56 @@
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[][] dots) {
+//         int answer = 0;
+
+//         for (int i = 0; i < dots.length; i++) {
+//             for (int j = i + 1; j < dots.length; j++) {
+//                 for (int k = 0; k < dots.length; k++) {
+//                     for (int l = k + 1; l < dots.length; l++) {
+//                         if (i != k && i != l && j != k && j != l) {
+//                             if ((dots[j][1] - dots[i][1]) * (dots[l][0] - dots[k][0]) == (dots[l][1] - dots[k][1]) * (dots[j][0] - dots[i][0])) {
+//                                 answer = 1;
+//                                 break;
+//                             }
+//                         }
+//                     }
+//                     if (answer == 1) {
+//                         break;
+//                     }
+//                 }
+//                 if (answer == 1) {
+//                     break;
+//                 }
+//             }
+//             if (answer == 1) {
+//                 break;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
-
-        for (int i = 0; i < dots.length; i++) {
-            for (int j = i + 1; j < dots.length; j++) {
-                for (int k = 0; k < dots.length; k++) {
-                    for (int l = k + 1; l < dots.length; l++) {
-                        if (i != k && i != l && j != k && j != l) {
-                            if ((dots[j][1] - dots[i][1]) * (dots[l][0] - dots[k][0]) == (dots[l][1] - dots[k][1]) * (dots[j][0] - dots[i][0])) {
-                                answer = 1;
-                                break;
-                            }
-                        }
-                    }
-                    if (answer == 1) {
-                        break;
-                    }
-                }
-                if (answer == 1) {
-                    break;
-                }
-            }
-            if (answer == 1) {
-                break;
-            }
+        
+        if (parallel(0, 1, 2, 3, dots) ||
+            parallel(0, 2, 1, 3, dots) ||
+            parallel(0, 3, 1, 2, dots)) {
+            answer = 1;
         }
         
         return answer;
     }
+    
+    private static boolean parallel(int a, int b, int c, int d, int[][] dots) {
+    int x = (dots[a][0] - dots[b][0]) * (dots[c][1] - dots[d][1]);
+    int y = (dots[a][1] - dots[b][1]) * (dots[c][0] - dots[d][0]);
+
+    return x == y; // 기울기가 같은지 확인
+  }
 }
