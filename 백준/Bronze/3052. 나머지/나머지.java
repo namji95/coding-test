929b937552a305ca523adb036dc47a79bac84919
NAMJI <138655542+namji95@users.noreply.github.com>
2024-12-02 12:47:12 +0900

    [Bronze II] Title: 나머지, Time: 168 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -2,21 +2,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int B = 42;
-        
-        solution1(B);
+        solution2();
     }
-
-
-    public static void solution1(int B) {
+    
+    public static void solution2() {
         Scanner in = new Scanner(System.in);
         Set<Integer> set = new HashSet<>();
-
+        
         for (int i = 0; i < 10; i++) {
-            int A  = in.nextInt();
-            set.add(A % B);
+            set.add(in.nextInt() % 42);
         }
-
+        
         System.out.println(set.size());
     }
 }
