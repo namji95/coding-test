98fce2fb0a8100cb79ca2a25af37b7eeef142a92
NAMJI <138655542+namji95@users.noreply.github.com>
2024-12-03 12:34:03 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -3,13 +3,14 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        solution2();
+        solution3();
     }
-
-    public static void solution2() throws IOException {
+    
+    public static void solution3() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
+        char[] c = s.toCharArray();
 
-        System.out.println(s.length());
+        System.out.println(c.length);
     }
 }
