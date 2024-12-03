f6fe210adc6c9d4b9d1ed5639e44699f2c1c8d25
NAMJI <138655542+namji95@users.noreply.github.com>
2024-12-03 12:27:23 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        solution1();
+    }
+
+    public static void solution1() {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        System.out.println(s.length());
+    }
+}
