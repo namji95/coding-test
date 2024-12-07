6d40a39b6b482f9e33e0d3e3fcade462a7772419
NAMJI <138655542+namji95@users.noreply.github.com>
2024-12-07 17:41:23 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 168 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        solution1();
+        solution2();
     }
 
-    public static void solution1() {
+    public static void solution2() {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         StringBuilder sb = new StringBuilder();
@@ -13,15 +13,8 @@ public class Main {
         String star = "*";
 
         for (int i = 1; i <= N; i++) {
-            for (int j = N-i; j > 0; j--) {
-                sb.append(space);
-            }
-            for (int k = i; k > 0; k--) {
-                sb.append(star);
-            }
-            sb.append("\n");
+            sb.append(space.repeat(N-i)).append(star.repeat(i)).append("\n");
         }
-
         System.out.println(sb);
     }
 }
