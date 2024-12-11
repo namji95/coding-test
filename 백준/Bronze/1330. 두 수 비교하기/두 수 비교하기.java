c44b5026cc894c86ee4479e199e86181105017cb
NAMJI <138655542+namji95@users.noreply.github.com>
2024-12-11 18:47:00 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -2,14 +2,15 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
-        
+    public static void main(String[] args) throws IOException {
+//        Scanner in = new Scanner(System.in);
+//        int A = in.nextInt();
+//        int B = in.nextInt();
+
 //        solution1(A, B);
 //        solution2(A, B);
-        solution3(A, B);
+//        solution3(A, B);
+        solution4();
     }
 
 //    public static void solution1(int A, int B) {
@@ -26,8 +27,18 @@ public class Main {
 //        String str = A > B ? ">" : ((A < B) ? "<" : "==");
 //        System.out.println(str);
 //    }
+//
+//    public static void solution3(int A, int B) {
+//        System.out.println(A > B ? ">" : ((A < B) ? "<" : "=="));
+//    }
 
-    public static void solution3(int A, int B) {
+    public static void solution4() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
         System.out.println(A > B ? ">" : ((A < B) ? "<" : "=="));
     }
 }
