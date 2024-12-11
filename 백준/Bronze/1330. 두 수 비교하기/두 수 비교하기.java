9cee42d0b746d4cbd16e9440788d6b6673d940c3
NAMJI <138655542+namji95@users.noreply.github.com>
2024-12-11 18:43:09 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,19 +1,33 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int A = in.nextInt();
-		int B = in.nextInt();
-		
-		in.close();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
         
-		if(A>B) System.out.println(">");
-		else if(A<B) System.out.println("<");
-		else System.out.println("==");
- 
-	}
- 
+//        solution1(A, B);
+//        solution2(A, B);
+        solution3(A, B);
+    }
+
+//    public static void solution1(int A, int B) {
+//        if (A > B) {
+//            System.out.println(">");
+//        } else if (A < B) {
+//            System.out.println("<");
+//        } else {
+//            System.out.println("==");
+//        }
+//    }
+//
+//    public static void solution2(int A, int B) {
+//        String str = A > B ? ">" : ((A < B) ? "<" : "==");
+//        System.out.println(str);
+//    }
+
+    public static void solution3(int A, int B) {
+        System.out.println(A > B ? ">" : ((A < B) ? "<" : "=="));
+    }
 }
