5b0b7920bf089647daa8a662cae8180ec5dee75e
NAMJI <138655542+namji95@users.noreply.github.com>
2024-12-12 12:40:58 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 20096 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,15 +1,15 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-
-        solution2(s);
+    public static void main(String[] args) throws IOException {
+        solution3();
     }
+    
+    public static void solution3() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-    public static void solution2(String s) {
-        StringTokenizer st = new StringTokenizer(s, " ");
         System.out.println(st.countTokens());
     }
 }
