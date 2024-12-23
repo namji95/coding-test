afd56ef4b15ccbbc3febd04db5ed9829f04f9b86
NAMJI <138655542+namji95@users.noreply.github.com>
2024-12-23 12:31:12 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13868 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,8 +1,21 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");
-	}
+//        solution1();
+        solution2();
+    }
+
+//    public static void solution1() {
+//        System.out.println("\\    /\\");
+//        System.out.println(" )  ( ')");
+//        System.out.println("(  /  )");
+//        System.out.println(" \\(__)|");
+//    }
+
+    public static void solution2() {
+        System.out.println(
+                "\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
 }
