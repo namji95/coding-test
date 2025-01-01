916680c9cd568143ade3cc0f81acc942955ab6fb
NAMJI <138655542+namji95@users.noreply.github.com>
2025-01-01 22:52:37 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,28 +1,57 @@
-import java.util.*;
-
+import java.util.Scanner;
+ 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int one = in.nextInt();
-        int two = in.nextInt();
-        int three = in.nextInt();
-
-        solution1(one, two, three);
-    }
-    
-    public static void solution1(int one, int two, int three) {
-        int answer = 0;
-        if (one == two && one == three) {
-            answer = 10000 + one * 1000;
-        } else if (one == two || one == three) {
-            answer = 1000 + one * 100;
-        } else if (two == three) {
-            answer = 1000 + two * 100;
-        } else {
-            int max = Math.max(one, Math.max(two, three));
-            answer = max * 100;
-        }
-
-        System.out.println(answer);
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		int a, b, c;
+		a = in.nextInt();
+		b = in.nextInt();
+		c = in.nextInt();
+ 
+		// 만약 모든 변수가 다른 경우
+		if (a != b && b != c && a != c) {
+			int max;
+			// 만약 a > b 라면
+			if (a > b) {
+				// c > a > b 라면
+				if (c > a) {
+					max = c;
+				} 
+				// a > (b, c)
+				else {
+					max = a;
+				}
+			}
+			// b > a 라면	
+			else {
+				// c > b > a 라면
+				if (c > b) {
+					max = c;
+				}
+				// b > (a, c)
+				else {
+					max = b;
+				}
+			}
+			System.out.println(max * 100);
+		}
+		// 적어도 한 쌍 이상의 서로 같은 변수가 존재할 경우
+		else {
+			// 3개의 변수가 모두 같은 경우
+			if (a == b && a == c) {
+				System.out.println(10000 + a * 1000);
+			}
+			else {
+				// a가 b혹은 c랑만 같은 경우
+				if(a == b || a == c) {
+					System.out.println(1000 + a * 100);
+				}
+				// b가 c랑 같은 경우
+				else {
+					System.out.println(1000 + b * 100);
+				}
+			}
+		}
+	}
 }
